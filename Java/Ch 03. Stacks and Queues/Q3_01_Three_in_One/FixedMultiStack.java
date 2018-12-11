238eb5202c8caaee343e485efcd73ec3c9c3fd4b
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 12:13:17 -0500

    Update FixedMultiStack.java

--- Java/Ch 03. Stacks and Queues/Q3_01_Three_in_One/FixedMultiStack.java
+++ Java/Ch 03. Stacks and Queues/Q3_01_Three_in_One/FixedMultiStack.java
@@ -1,3 +1,17 @@
+/*
+Like many problems, this one somewhat depends on how well we'd like to support these stacks. If we're
+okay with simply allocating a fixed amount of space for each stack, we can do that. This may mean though
+that one stack runs out of space, while the others are nearly empty.
+Alternatively, we can be flexible in our space allocation, but this significantly increases the complexity of
+the problem.
+Approach 1: Fixed Division
+We can divide the array in three equal parts and allow the individual stack to grow in that limited space.
+Note: We will use the notation "[" to mean inclusive of an end point and "(" to mean exclusive of an end
+point.
+For stack 1, we will use [0, X).
+For stack 2, we will use [ X , 2X ) .
+For stack 3, we will use [ 2X , n) .
+*/
 package Q3_01_Three_in_One;
 
 import java.util.EmptyStackException;
