74f5cb310d066c1f229d83e40b601f26d314b954
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 12:43:27 -0500

    Update StackWithMin2.java

--- Java/Ch 03. Stacks and Queues/Q3_02_Stack_Min/StackWithMin2.java
+++ Java/Ch 03. Stacks and Queues/Q3_02_Stack_Min/StackWithMin2.java
@@ -1,3 +1,14 @@
+/*
+There's just one issue with this: if we have a large stack, we waste a lot of space by keeping track of the min
+for every single element. Can we do better?
+
+We can (maybe) do a bit better than this by using an additional stack which keeps track of the mins.
+Why might this be more space efficient? Suppose we had a very large stack and the first element inserted
+happened to be the minimum. In the first solution, we would be keeping n integers, where n is the size of
+the stack. In the second solution though, we store just a few pieces of data: a second stack with one element
+and the members within this stack.
+*/
+
 package Q3_02_Stack_Min;
 
 import java.util.Stack;
