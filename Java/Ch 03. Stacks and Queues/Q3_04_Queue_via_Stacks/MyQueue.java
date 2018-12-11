8d02134a1fedf686a72f8364633199ca4b6b3346
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 14:32:31 -0500

    Update MyQueue.java

--- Java/Ch 03. Stacks and Queues/Q3_04_Queue_via_Stacks/MyQueue.java
+++ Java/Ch 03. Stacks and Queues/Q3_04_Queue_via_Stacks/MyQueue.java
@@ -1,3 +1,18 @@
+/*
+Since the major difference between a queue and a stack is the order (first-in first-out vs. last-in first-out), we
+know that we need to modify peek() and pop() to go in reverse order. We can use our second stack to
+reverse the order of the elements (by popping sl and pushing the elements on to s2). In such an implementation,
+on each peek() and pop() operation, we would pop everything from sl onto s2, perform
+the peek/ pop operation, and then push everything back.
+This will work, but if two pop/ peeks are performed back-to-back, we're needlessly moving elements. We
+can implement a "lazy" approach where we let the elements sit in s2 until we absolutely must reverse the
+elements.
+In this approach, stackNewest has the newest elements on top and stackOldest has the oldest
+elements on top. When we dequeue an element, we want to remove the oldest element first, and so we
+dequeue from stackOldest. If stackOldest is empty, then we want to transfer all elements from
+stackNewest into this stack in reverse order. To insert an element, we push onto stackNewest, since it
+has the newest elements on top.
+*/
 package Q3_04_Queue_via_Stacks;
 
 import java.util.Stack;
