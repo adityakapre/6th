24b5bfef36c2b158bf196be0862538dd0b8c3a68
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 10:47:01 -0500

    Update QuestionA.java

--- Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/QuestionA.java
+++ Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/QuestionA.java
@@ -1,3 +1,15 @@
+/*Solution #2: Recursive
+This algorithm recurses through the linked list. When it hits the end, the method passes back a counter set
+to 0. Each parent call adds 1 to this counter. When the counter equals k, we know we have reached the kth
+to last element of the linked list.
+Implementing this is short and sweet-provided we have a way of"passing back" an integer value through
+the stack. Unfortunately, we can't pass back a node and a counter using normal return statements. So how
+do we handle this?
+Approach A: Don't Return the Element.
+One way to do this is to change the problem to simply printing the kth to last element. Then, we can pass
+back the value of the counter simply through return values.
+*/
+
 package Q2_02_Return_Kth_To_Last;
 
 import CtCILibrary.*;
