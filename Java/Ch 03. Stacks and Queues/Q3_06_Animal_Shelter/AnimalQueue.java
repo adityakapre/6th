f0e8253a4b87377aad59e538722d529379963d0e
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 15:01:44 -0500

    Update AnimalQueue.java

--- Java/Ch 03. Stacks and Queues/Q3_06_Animal_Shelter/AnimalQueue.java
+++ Java/Ch 03. Stacks and Queues/Q3_06_Animal_Shelter/AnimalQueue.java
@@ -1,3 +1,19 @@
+/*
+We could explore a variety of solutions to this problem. For instance, we could maintain a single queue.
+This would make dequeueAny easy, but dequeueDog and dequeueCat would require iteration through
+the queue to find the first dog or cat. This would increase the complexity of the solution and decrease the
+efficiency.
+An alternative approach that is simple, clean and efficient is to simply use separate queues for dogs and
+cats, and to place them within a wrapper class called AnimalQueue. We then store some sort of timestamp
+to mark when each animal was enqueued. When we call dequeueAny, we peek at the heads of both the
+dog and cat queue and return the oldest.
+
+It is important that Dog and Cat both inherit from an Animal class since dequeueAny() needs to be able
+to support returning both Dog and Cat objects.
+If we wanted, order could be a true timestamp with the actual date and time. The advantage of this is that
+we wouldn't have to set and maintain the numerical order. If we somehow wound up with two animals with
+the same timestamp, then (by definition) we don't have an older animal and we could return either one.
+*/
 package Q3_06_Animal_Shelter;
 
 import java.util.LinkedList;
