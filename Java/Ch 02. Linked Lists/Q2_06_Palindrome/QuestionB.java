0af8427e4295464198d2c707a05286631970ff92
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 11:35:03 -0500

    Update QuestionB.java

--- Java/Ch 02. Linked Lists/Q2_06_Palindrome/QuestionB.java
+++ Java/Ch 02. Linked Lists/Q2_06_Palindrome/QuestionB.java
@@ -1,3 +1,19 @@
+/*
+Solution #2: Iterative Approach
+We want to detect linked lists where the front half of the list is the reverse of the second half. How would we
+do that? By reversing the front half of the list. A stack can accomplish this.
+We need to push the first half of the elements onto a stack. We can do this in two different ways, depending
+on whether or not we know the size of the linked list.
+If we know the size of the linked list, we can iterate through the first half of the elements in a standard for
+loop, pushing each element onto a stack. We must be careful, of course, to handle the case where the length
+of the linked list is odd.
+If we don't know the size of the linked list, we can iterate through the linked list, using the fast runner/ slow
+runner technique described in the beginning of the chapter. At each step in the loop, we push the data from
+the slow runner onto a stack. When the fast runner hits the end of the list, the slow runner will have reached
+the middle of the linked list. By this point, the stack will have all the elements from the front of the linked
+list, but in reverse order.
+*/
+
 package Q2_06_Palindrome;
 
 import CtCILibrary.LinkedListNode;
