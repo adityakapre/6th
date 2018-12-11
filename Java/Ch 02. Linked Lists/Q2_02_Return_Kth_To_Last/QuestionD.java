e5577966eb7405d45f89bc0361b9f475644eea1e
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 10:52:47 -0500

    Update QuestionD.java

--- Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/QuestionD.java
+++ Java/Ch 02. Linked Lists/Q2_02_Return_Kth_To_Last/QuestionD.java
@@ -1,3 +1,9 @@
+/*Solution #3: Iterative
+A more optimal, but less straightforward, solution is to implement this iteratively. We can use two pointers,
+pl and p2. We place them k nodes apart in the linked list by putting p2 at the beginning and moving pl
+k nodes into the list. Then, when we move them at the same pace, pl will hit the end of the linked list after
+LENGTH - k steps. At that point, p2 will be LENGTH - k nodes into the list, or k nodes from the end.*/
+
 package Q2_02_Return_Kth_To_Last;
 
 import CtCILibrary.*;
