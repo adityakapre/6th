f5e97a99dea0a2bd182a4da14476e8b4f396da54
comeon.getit <adityakapre2009@gmail.com>
2018-12-11 11:02:32 -0500

    Update Question.java

--- Java/Ch 02. Linked Lists/Q2_03_Delete_Middle_Node/Question.java
+++ Java/Ch 02. Linked Lists/Q2_03_Delete_Middle_Node/Question.java
@@ -1,3 +1,10 @@
+/*In this p roblem, you are not given access to the head of the linked list. You only have access to that node.
+The solution is simply to copy the data from the next node over to the current node, and then to delete the
+next node.
+Note that this problem cannot be solved if the node to be deleted is the last node in the linked list. That's
+okay-your interviewer wants you to point that out, and to discuss how to handle this case. You could, for
+example, consider marking the node as dummy.*/
+
 package Q2_03_Delete_Middle_Node;
 
 import CtCILibrary.AssortedMethods;
