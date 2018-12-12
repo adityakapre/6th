486511d1b6e9f9de3bee129da8637d1b1a306b2e
comeon.getit <adityakapre2009@gmail.com>
2018-12-12 11:45:24 -0500

    Update Question.java

--- Java/Ch 04. Trees and Graphs/Q4_07_Build_Order/EdgeRemoval/Question.java
+++ Java/Ch 04. Trees and Graphs/Q4_07_Build_Order/EdgeRemoval/Question.java
@@ -3,7 +3,44 @@ package Q4_07_Build_Order.EdgeRemoval;
 import java.util.ArrayList;
 
 public class Question {
+	
+	public static void main(String[] args) {
+		String[] projects = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+		String[][] dependencies = {
+				{"a", "b"},
+				{"b", "c"},
+				{"a", "c"},
+				{"a", "c"},
+				{"d", "e"},
+				{"b", "d"},
+				{"e", "f"},
+				{"a", "f"},
+				{"h", "i"},
+				{"h", "j"},
+				{"i", "j"},
+				{"g", "j"}};
+		String[] buildOrder = buildOrderWrapper(projects, dependencies);
+		if (buildOrder == null) {
+			System.out.println("Circular Dependency.");
+		} else {
+			for (String s : buildOrder) {
+				System.out.println(s);
+			}
+		}
+	}
 
+	public static String[] buildOrderWrapper(String[] projects, String[][] dependencies) {
+		Project[] buildOrder = findBuildOrder(projects, dependencies);
+		if (buildOrder == null) return null;
+		String[] buildOrderString = convertToStringList(buildOrder);
+		return buildOrderString;
+	}
+	
+	public static Project[] findBuildOrder(String[] projects, String[][] dependencies) {
+		Graph graph = buildGraph(projects, dependencies);
+		return orderProjects(graph.getNodes());
+	}
+	
 	/* Build the graph, adding the edge (a, b) if b is dependent on a. 
 	 * Assumes a pair is listed in “build order”. The pair (a, b) in 
 	 * dependencies indicates that b depends on a and a must be built
@@ -21,18 +58,6 @@ public class Question {
 		}
 		
 		return graph;
-	}
-	
-	/* A helper function to insert projects with zero dependencies 
-	 * into the order array, starting at index offset. */
-	public static int addNonDependent(Project[] order, ArrayList<Project> projects, int offset) {
-		for (Project project : projects) {
-			if (project.getNumberDependencies() == 0) {
-				order[offset] = project;
-				offset++;
-			}
-		}
-		return offset;
 	}
 	
 	public static Project[] orderProjects(ArrayList<Project> projects) {
@@ -66,6 +91,18 @@ public class Question {
 		return order;
 	}
 	
+	/* A helper function to insert projects with zero dependencies 
+	 * into the order array, starting at index offset. */
+	public static int addNonDependent(Project[] order, ArrayList<Project> projects, int offset) {
+		for (Project project : projects) {
+			if (project.getNumberDependencies() == 0) {
+				order[offset] = project;
+				offset++;
+			}
+		}
+		return offset;
+	}
+	
 	public static String[] convertToStringList(Project[] projects) {
 		String[] buildOrder = new String[projects.length];
 		for (int i = 0; i < projects.length; i++) {
@@ -74,42 +111,4 @@ public class Question {
 		return buildOrder;
 	}
 	
-	public static Project[] findBuildOrder(String[] projects, String[][] dependencies) {
-		Graph graph = buildGraph(projects, dependencies);
-		return orderProjects(graph.getNodes());
-	}
-	
-	
-	public static String[] buildOrderWrapper(String[] projects, String[][] dependencies) {
-		Project[] buildOrder = findBuildOrder(projects, dependencies);
-		if (buildOrder == null) return null;
-		String[] buildOrderString = convertToStringList(buildOrder);
-		return buildOrderString;
-	}
-	
-	public static void main(String[] args) {
-		String[] projects = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-		String[][] dependencies = {
-				{"a", "b"},
-				{"b", "c"},
-				{"a", "c"},
-				{"a", "c"},
-				{"d", "e"},
-				{"b", "d"},
-				{"e", "f"},
-				{"a", "f"},
-				{"h", "i"},
-				{"h", "j"},
-				{"i", "j"},
-				{"g", "j"}};
-		String[] buildOrder = buildOrderWrapper(projects, dependencies);
-		if (buildOrder == null) {
-			System.out.println("Circular Dependency.");
-		} else {
-			for (String s : buildOrder) {
-				System.out.println(s);
-			}
-		}
-	}
-
 }
