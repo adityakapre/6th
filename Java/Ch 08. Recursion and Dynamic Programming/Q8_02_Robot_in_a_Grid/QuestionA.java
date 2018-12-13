794c8bfcc38d3beeeb2866ce79755e725d150d31
comeon.getit <adityakapre2009@gmail.com>
2018-12-13 16:29:58 -0500

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_02_Robot_in_a_Grid/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_02_Robot_in_a_Grid/QuestionA.java
@@ -1,3 +1,23 @@
+/*
+If we picture this grid, the only way to move to spot ( r, c) is by moving to one of the adjacent spots:
+(r-1, c) or (r, c-1). So, we need to find a path to either (r-1, c) or (r, c-1).
+How do we find a path to those spots? To find a path to (r-1, c) or (r, c -1), we need to move to one
+of its adjacent cells. So, we need to find a path to a spot adjacent to (r-1, c), which are coordinates
+(r-2, c) and (r-1, c-1). or a spot adjacent to (r, c-1). which are soots (r-1, c-1) and (r, c-2).
+Observe that we list the point (r-1, c-1) twice; we'll discuss that issue later.
+
+I Tip: A lot of people use the variable names x and y when dealing with two-dimensional arrays.
+This can actually cause some bugs. People tend to think about x as the first coordinate in the
+matrix and y as the second coordinate (e.g., matrix[x] [y]). But, this isn't really correct. The
+first coordinate is usually thought of as the row number, which is in fact they value (it goes vertically!).
+You should write matrix[y] [x]. Or, just make your life easier by using r (row) and c
+(column) instead.
+
+So then, to find a path from the origin, we just work backwards like this. Starting from the last cell, we try to
+find a path to each of its adjacent cells. The recursive code below implements this algorithm.
+
+This solution is O (2 raised to (r+c) ), since each path has r+c steps and there are two choices we can make at each step.
+*/
 package Q8_02_Robot_in_a_Grid;
 
 import java.util.ArrayList;
