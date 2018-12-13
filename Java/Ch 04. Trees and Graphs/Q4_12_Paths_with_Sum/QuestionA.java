543081f816f60cad9cf6eff19b63db590754752b
comeon.getit <adityakapre2009@gmail.com>
2018-12-13 14:30:41 -0500

    Update QuestionA.java

--- Java/Ch 04. Trees and Graphs/Q4_12_Paths_with_Sum/QuestionA.java
+++ Java/Ch 04. Trees and Graphs/Q4_12_Paths_with_Sum/QuestionA.java
@@ -1,3 +1,10 @@
+/*
+One option is the brute force approach.
+Solution #1: Brute Force
+In the brute force approach, we just look at all possible paths. To do this, we traverse to each node. At each
+node, we recursively try all paths downwards, tracking the sum as we go. As soon as we hit our target sum,
+we increment the total.
+*/
 package Q4_12_Paths_with_Sum;
 
 import CtCILibrary.TreeNode;
