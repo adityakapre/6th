1dcaaacf8ace52254e1d86e433c1cd7dddb88668
comeon.getit <adityakapre2009@gmail.com>
2018-12-18 22:26:55 -0500

    Update Traversals.java

--- Java/Ch 04. Trees and Graphs/Introduction/Traversals.java
+++ Java/Ch 04. Trees and Graphs/Introduction/Traversals.java
@@ -33,6 +33,13 @@ public class Traversals {
 		}
 	}	
 	
+	public static int getHeight(TreeNode root) {
+		if (root == null) {
+			return -1;
+		}
+		return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+	}
+	
 	public static void main(String[] args) {
 		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
