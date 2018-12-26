348db8a14f11d743960d4cab377418b7fb18e867
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 13:43:37 -0500

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionA.java
@@ -1,3 +1,34 @@
+/*
+Solution #1: Recursion
+This problem is a good candidate for the Base Case and Build approach. Imagine that we are trying to find
+all subsets of a set like S = {a1, a2, ••• , an}. We can start with the Base Case.
+Base Case: n = 0.
+There is just one subset of the empty set: {}.
+Case:n = 1.
+There are two subsets of the set {a}: {}, {aJ.
+Case:n = 2.
+There are four subsets of the set {a1, a2}: {}, {a1}, {a2), {a1, a 2}.
+Case:n = 3.
+Now here's where things get interesting. We want to find a way of generating the solution for n=3 based
+on the prior solutions.
+
+What is the difference between the solution for n=3 and the solution for n=2? Let's look at this more
+deeply:
+P(2) = {}, {a1}, {a2}, {a1, a2}
+P(3) = {}, {a1}, {a2}, {a3}, {a1, a2}, {a1, a3}, {a2, a3}, {a1, a2, a3}
+The difference between these solutions is that P (2) is missing all the subsets containing a3
+P(3) - P(2) = {a3}, {a1, a3}, {a2, a3}, {a1, a2, a3}
+How can we use P (2) to create P(3)? We can simply clone the subsets in P(2) and add a3 to them:
+
+P(2)  = {}, {a1}, {a2}, {a1, a2}
+P(2) + a3 = {a3}, {a1, a3}, {a2 , a3}, {a1, a2, a 3}
+
+When merged together, the lines above make P (3).
+
+Case:n > 0
+Generating P (n) for the general case is just a simple generalization of the above steps. We compute
+P (n-1), clone the results, and then add a n to each of these cloned sets.
+*/
 package Q8_04_Power_Set;
 
 import java.util.*;
