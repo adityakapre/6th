4f0029af0c159d8f0f223fd17d0030345e3dd70f
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 15:37:30 -0500

    Update QuestionB.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_07_Permutations_Without_Dups/QuestionB.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_07_Permutations_Without_Dups/QuestionB.java
@@ -1,3 +1,27 @@
+/*
+Approach 2: Building from permutations of all n-1 character substrings.
+Base Case: single-character strings
+The only permutation of a1 is the string a1. So:
+P(a1) = a1
+Case: two-character strings
+P(a1a2) = a1a2 and a2a1 
+P(a2a3) = a2a3 and a3a2
+P(a1a3) = a1a3 and a3a1
+
+Case: three-character strings
+Here is where the cases get more interesting. How can we generate all permutations of three-character
+strings, such as a1a2a3, given the permutations of two-character strings?
+Well, in essence, we just need to "try" each character as the first character and then append the permutations.
+P(a1a2a3) = {a1 + P(a2a3)} + {a2 + P(a1a3)} + {a3 + P(a1a2)}
+{a1 + P(a2a3)} -> a1a2a3, a1a3a2
+{a2 + P(a1a3)} -> a2a1a3, a2a3a1
+{a3 + P(a1a2)} -> a3a1a2, a3a2a1
+
+Now that we can generate all permutations of three-character strings, we can use this to generate permutations
+of four-character strings.
+P(a1a2a3a4) = {a1 + P(a2a3a4)} + {a2 + P(a1a3a4)} + {a3, + P(a1a2a4)} + {a4 + P(a1a2a3)}
+This is now a fairly straightforward algorithm to implement.
+*/
 package Q8_07_Permutations_Without_Dups;
 
 import java.util.*;
