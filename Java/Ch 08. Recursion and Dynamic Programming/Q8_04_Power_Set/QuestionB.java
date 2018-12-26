d4b0bce4332c33f18d13541693b1755ce56c80b4
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 13:55:07 -0500

    Update QuestionB.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionB.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionB.java
@@ -1,3 +1,16 @@
+/*
+Solution #2: Combinatorics
+While there's nothing wrong with the above solution, there's another way to approach it.
+Recall that when we're generating a set, we have two choices for each element: (1) the element is in the set
+(the "yes" state) or (2) the element is not in the set (the "no" state). This means that each subset is a sequence
+of yeses/nos-e.g., "yes, yes, no, no, yes, no"
+This gives us 2 raisedTo n possible subsets. How can we iterate through all possible sequences of"yes" /"no" states for
+all elements? If each "yes" can be treated as a 1 and each "no" can be treated as a 0, then each subset can be
+represented as a binary string.
+Generating all subsets, then, really just comes down to generating all binary numbers (that is, all integers).
+We iterate through all numbers from 0 to 2 raisedTo n (exclusive) and translate the binary representation of the
+numbers into a set. Easy!
+*/
 package Q8_04_Power_Set;
 
 import java.util.*;
