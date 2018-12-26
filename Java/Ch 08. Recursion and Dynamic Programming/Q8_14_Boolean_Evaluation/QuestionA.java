297a260c17c41eb4e4695e468a42130ada505945
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 16:46:31 -0500

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_14_Boolean_Evaluation/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_14_Boolean_Evaluation/QuestionA.java
@@ -1,3 +1,44 @@
+/*
+Brute Force
+Consider an expression like 0^0&0^1|1 and the target result true. How can we break down
+countEval(0^0&0^1|1, true) into smaller problems?
+We could just essentially iterate through each possible place to put a parenthesis.
+
+countEval(0^0&0^1|1, true)=
+countEval(0^0&0^1|1, where paren around char 1, true)
++ countEval(0^0&0^1|1, where paren around char 3, true)
++ countEval(0^0&0^1|1, where paren around char 5, true)
++ countEval(0^0&0^1|1, where paren around char 7, true)
+
+Now what? Let's look at just one of those expressions-the paren around char 3. This gives us (0^0)&(0^1).
+In order to make that expression true, both the left and right sides must be true. So:
+left= "0^0"
+right = "0^1|1"
+countEval(left & right, true)= countEval(left, true) * countEval(right, true)
+
+The reason we multiply the results of the left and right sides is that each result from the two sides can be
+paired up with each other to form a unique combination.
+Each of those terms can now be decomposed into smaller problems in a similar process.
+
+What happens when we have an "|"(OR)? Or an"^"(XOR)?
+If it's an OR, then either the left or the right side must be true-or both.
+countEval(left | right, true) = countEval(left, true) * countEval(right, false)
++ countEval(left, false) * countEval(right, true)
++ countEval(left, true) * countEval(right, true)
+
+If it's an XOR, then the left or the right side can be true, but not both.
+countEval(left ^ right, true)= countEval(left, true) * countEval(right, false)
++ countEval(left, false) * countEval(right, true)
+
+What if we were trying to make the result false instead? We can switch up the logic from above:
+countEval(left & right, false) = countEval(left, true) * countEval(right, false)
++ countEval(left, false) * countEval(right, true) + countEval(left, false) * countEval(right, false)
+
+countEval(left | right, false) = countEval(left, false) * countEval(right, false)
+
+countEval(left ^ right, false) = countEval(left, false) * countEval(right, false) + countEval(left, false) * countEval(right, false)
++ countEval(left, true) * countEval(right, true)
+*/
 package Q8_14_Boolean_Evaluation;
 
 public class QuestionA {
