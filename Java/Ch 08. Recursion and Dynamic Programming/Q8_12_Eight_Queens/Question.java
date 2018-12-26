447b1a9eaba1c45ebc332ba608c1dd230dcd9b7f
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 16:14:59 -0500

    Update Question.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_12_Eight_Queens/Question.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_12_Eight_Queens/Question.java
@@ -1,3 +1,38 @@
+
+/*
+We have eight queens which must be lined up on an 8x8 chess board such that none share the same row,
+column or diagonal. So, we know that each row and column (and diagonal) must be used exactly once .
+
+Picture the queen that is placed last, which we'll assume is on row 8. (This is an okay assumption to make
+since the ordering of placing the queens is irrelevant.) On which cell in row 8 is this queen? There are eight
+possibilities, one for each column.
+
+So if we want to know all the valid ways of arranging 8 queens on an 8x8 chess board, it would be:
+ways to arrange 8 queens on an 8x8 board=
+ways to arrange 8 queens on an 8x8 board with queen at (7, 0) +
+ways to arrange 8 queens on an 8x8 board with queen at (7, 1) +
+ways to arrange 8 queens on an 8x8 board with queen at (7, 2) + 
+ways to arrange 8 queens on an 8x8 board with queen at (7, 3) +
+ways to arrange 8 queens on an 8x8 board with queen at (7, 4) +
+ways to arrange 8 queens on an 8x8 board with queen at (7, 5) +
+ways to arrange 8 queens on an 8x8 board with queen at (7, 6) +
+ways to arrange 8 queens on an 8x8 board with queen at (7, 7)
+
+We can compute each one of these using a very similar approach:
+ways to arrange 8 queens on an 8x8 board with queen at (7, 3)
+ways to with queens at (7, 3) and (6, 0) +
+ways to with queens at (7, 3) and (6, 1) +
+ways to with queens at (7, 3) and (6, 2) +
+ways to with queens at (7, 3) and (6, 4) +
+ways to with queens at (7, 3) and (6, 5) +
+ways to with queens at (7, 3) and (6, 6) +
+ways to with queens at (7, 3) and (6, 7)
+Note that we don't need to consider combinations with queens at (7, 3) and (6,3) since it is a 
+violation of the requirement that every queen is in its own row, column and diagonal.
+
+Observe that since each row can only have one queen, we don't need to store our board as a full 8x8 matrix.
+We only need a single array where column[r] = c indicates that row r has a queen at column c.
+*/
 package Q8_12_Eight_Queens;
 
 import java.util.ArrayList;
