6d8413967bb92b333ec5ddb3f7855d0d0db9c9a8
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 14:58:38 -0500

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_07_Permutations_Without_Dups/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_07_Permutations_Without_Dups/QuestionA.java
@@ -1,3 +1,30 @@
+/*
+Approach 1: Building from permutations of first n-1 characters.
+Base Case: permutations of first character substring
+The only permutation of a1 is the string a1. So:
+P(a1) = a1
+
+Case: permutations of a1 a2
+P(a1a2) = a1a2 and a2a1
+
+Case: permutations of a1a2a3
+P(a1a2a3) = a1a2a3, a1a3a2, a2a1a3, a2a3a1, a3a1a2, a3a2a1 ,
+
+Case: permutations of a1a2a3a4
+This is the first interesting case. How can we generate permutations of a1a2a3a4 from a1a2a3 ?
+Each permutation of a1a2a3a4 represents an ordering of a1a2a3. For example, a2a4a1a3 represents the order
+a2a1a3.
+Therefore, if we took all the permutations of a1a2a3 and added a4 into all possible locations, we would get all
+permutations of a1a2a3a4.
+a1a2a3 -> a4a1a2a3 a1a4a2a3 a1a2a4a3 a1a2a3a4
+a1a3a2 -> a4a1a3a2 a1a4a3a2 a1a3a4a2 a1a3a2a4
+a3a1a2 -> a4a3a1a2 a3a4a1a2 a3a1a4a2 a3a1a2a4
+a2a1a3 -> a4a2a1a3 a2a4a1a3 a2a1a4a3 a2a1a3a4
+a2a3a1 -> a4a2a3a1 a2a4a3a1 a2a3a4a1 a2a3a1a4
+a3a2a1 -> a4a3a2a1 a3a4a2a1 a3a2a4a1 a3a2a1a4
+
+We can now implement this algorithm recursively.
+*/
 package Q8_07_Permutations_Without_Dups;
 
 import java.util.*;
