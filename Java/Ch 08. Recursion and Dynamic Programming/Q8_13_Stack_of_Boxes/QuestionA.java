f4f30949c8bc49b0e550e3095ac52876d15c622e
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 16:22:11 -0500

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_13_Stack_of_Boxes/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_13_Stack_of_Boxes/QuestionA.java
@@ -1,3 +1,19 @@
+/*
+Solution#1
+Imagine we had the following boxes: b1 , b2 , ••• , bn. The biggest stack that we can build with all the
+boxes equals the max of (biggest stack with bottom b1, biggest stack with bottom b2 , ••• , biggest stack
+with bottom bn). That is, if we experimented with each box as a bottom and built the biggest stack possible
+with each, we would find the biggest stack possible.
+But, how would we find the biggest stack with a particular bottom? Essentially the same way. We experiment
+with different boxes for the second level, and so on for each level.
+Of course, we only experiment with valid boxes. If b5 is bigger than b1, then there's no point in trying to
+build a stack that looks like {b1 , b 5 , ••• }. We already know b1 can't be below b5•
+We can perform a small optimization here. The requirements of this problem stipulate that the lower boxes
+must be strictly greater than the higher boxes in all dimensions. Therefore, if we sort (descending order) the
+boxes on a dimension-any dimension-then we know we don't have to look backwards in the list. The
+box b1 cannot be on top of box b5, since its height (or whatever dimension we sorted on) is greater than
+b5's height.
+*/
 package Q8_13_Stack_of_Boxes;
 
 import java.util.ArrayList;
