17ab64fb112b9b00d461c9231de1dae27f2fe9cf
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 15:50:19 -0500

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_09_Parens/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_09_Parens/QuestionA.java
@@ -1,3 +1,25 @@
+/*
+Our first thought here might be to apply a recursive approach where we build the solution for f(n) by
+adding pairs of parentheses to f(n-1). That's certainly a good instinct.
+Let's consider the solution for n = 3:
+(()()) ((())) ()(())
+How might we build this from n = 2?
+(()) ()()
+(())() () () ()
+We can do this by inserting a pair of parentheses inside every existing pair of parentheses, as well as one
+at the beginning of the string. Any other places that we could insert parentheses, such as at the end of the
+string, would reduce to the earlier cases.
+So, we have the following:
+(()) -> (()()) // inserted pair after 1st left paren 
+-> ((())) // inserted pair after 2nd left paren 
+-> ()(()) // inserted pair at beginning of string
+() () -> (())() // inserted pair after 1st left paren 
+-> ()(()) // ins erted pair after 2nd left paren 
+-> () () () // inserted pair at beginning of string
+But wait-we have some duplicate pairs listed. The string () (()) is listed twice.
+If we're going to apply this approach, we'll need to check for duplicate values before adding a string to our
+list.
+*/
 package Q8_09_Parens;
 
 import java.util.HashSet;
