024de34d70ba3e1c98188098a1d47130e772f77d
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 11:29:40 -0500

    Update QuestionA.java

--- Java/Ch 01. Arrays and Strings/Q1_05_One_Away/QuestionA.java
+++ Java/Ch 01. Arrays and Strings/Q1_05_One_Away/QuestionA.java
@@ -3,6 +3,7 @@ There is a "brute force" algorithm to do this. We could check all possible strin
 testing the removal of each character (and comparing), testing the replacement of each character (and
 comparing), and then testing the insertion of each possible character (and comparing).
 That would be too slow, so let's not bother with implementing it.
+
 This is one of those problems where it's helpful to think about the "meaning" of each of these operations.
 What does it mean for two strings to be one insertion, replacement, or removal away from each other?
 • Replacement: Consider two strings, such as bale and pale, that are one replacement away. Yes, that
@@ -12,20 +13,22 @@ they are different only in one place.
 strings, they would be identical-except for a shift at some point in the strings.
 • Removal: The strings apple and aple are also one removal away, since removal is just the inverse of
 insertion.
+
 We can go ahead and implement this algorithm now. We'll merge the insertion and removal check into one
 step, and check the replacement step separately.
 Observe that you don't need to check the strings for insertion, removal, and replacement edits. The lengths
 of the strings will indicate which of these you need to check.
 
-This algorithm (and almost any reasonable algorithm) takes O ( n) time, where n is the length of the shorter
+This algorithm (and almost any reasonable algorithm) takes O (n) time, where n is the length of the shorter
 string.
-I Why is the runtime dictated by the shorter string instead of the longer string? If the strings are
+
+Why is the runtime dictated by the shorter string instead of the longer string? If the strings are
 the same length (plus or minus one character), then it doesn't matter whether we use the longer
 string or the shorter string to define the runtime. If the strings are very different lengths, then the
-algorithm will terminate in 0( 1) time. One really, really long string therefore won't significantly
+algorithm will terminate in 0(1) time. One really, really long string therefore won't significantly
 extend the runtime. It increases the runtime only if both strings are long.
 
-We might notice that the code for one Edi tReplace is very simtlar to that for one Edi tinsert. We can
+We might notice that the code for one EditReplace is very simtlar to that for one Editinsert. We can
 merge them into one method.
 */
 package Q1_05_One_Away;
