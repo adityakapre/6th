dfd25cd04e562a6142971bc406b06b7e3fe4b9e8
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 16:06:40 -0500

    Update Question.java

--- Java/Ch 04. Trees and Graphs/Q4_07_Build_Order/EdgeRemoval/Question.java
+++ Java/Ch 04. Trees and Graphs/Q4_07_Build_Order/EdgeRemoval/Question.java
@@ -2,6 +2,31 @@ package Q4_07_Build_Order.EdgeRemoval;
 
 import java.util.ArrayList;
 
+class Graph {
+	private ArrayList<Project> nodes = new ArrayList<Project>();
+	private HashMap<String, Project> map = new HashMap<String, Project>();
+	
+	public Project getOrCreateNode(String name) {
+		if (!map.containsKey(name)) {
+			Project node = new Project(name);
+			nodes.add(node);
+			map.put(name, node);
+		}
+		
+		return map.get(name);
+	}
+	
+	public void addEdge(String startName, String endName) {
+		Project start = getOrCreateNode(startName);
+		Project end = getOrCreateNode(endName);
+		start.addNeighbor(end);
+	}
+	
+	public ArrayList<Project> getNodes() {
+		return nodes;
+	}
+}
+
 public class Question {
 	
 	public static void main(String[] args) {
