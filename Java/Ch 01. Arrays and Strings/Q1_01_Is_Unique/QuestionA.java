5382d423c82ad9a014eb571b995e99942979eb77
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 10:43:18 -0500

    Update QuestionA.java

--- Java/Ch 01. Arrays and Strings/Q1_01_Is_Unique/QuestionA.java
+++ Java/Ch 01. Arrays and Strings/Q1_01_Is_Unique/QuestionA.java
@@ -1,3 +1,15 @@
+/*
+You should first ask your interviewer if the string is an ASCII string or a Unicode string. Asking this question
+will show an eye for detail and a solid foundation in computer science. We'll assume for simplicity the character
+set is ASCII. If this assumption is not valid, we would need to increase the storage size.
+One solution is to create an array of boolean values, where the flag at index i indicates whether character
+i in the alphabet is contained in the string. The second time you see this character you can immediately
+return false.
+We can also immediately return false if the string length exceeds the number of unique characters in the
+alphabet. After all, you can't form a string of 280 unique characters out of a 128-character alphabet.
+It's also okay to assume 256 characters. This would be the case in extended ASCII. You should
+clarify your assumptions with your interviewer.
+*/
 package Q1_01_Is_Unique;
 
 public class QuestionA {
@@ -7,7 +19,7 @@ public class QuestionA {
 		}
 		boolean[] char_set = new boolean[128];
 		for (int i = 0; i < str.length(); i++) {
-			int val = str.charAt(i);
+			int val = str.charAt(i);	//remember this string api to get each string character
 			if (char_set[val]) return false;
 			char_set[val] = true;
 		}
