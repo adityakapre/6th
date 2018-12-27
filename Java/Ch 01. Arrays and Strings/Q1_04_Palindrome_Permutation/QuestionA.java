a32fe5958bf23287e0e2d6a021a6ce18edd0c05e
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 11:16:39 -0500

    Update QuestionA.java

--- Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/QuestionA.java
+++ Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/QuestionA.java
@@ -26,6 +26,18 @@ an odd count.
 package Q1_04_Palindrome_Permutation;
 
 public class QuestionA {	
+	
+	public static int[] buildCharFrequencyTable(String phrase) {
+		int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+		for (char c : phrase.toCharArray()) {
+			int x = getCharNumber(c);
+			if (x != -1) {
+				table[x]++;
+			}
+		}
+		return table;
+	}
+	
 	public static boolean checkMaxOneOdd(int[] table) {
 		boolean foundOdd = false;
 		for (int count : table) {
