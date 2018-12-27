0507ffbf76eea26e3cb0dc7148a0690f0d7297ad
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 11:21:12 -0500

    Update QuestionB.java

--- Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_04_Palindrome_Permutation/QuestionB.java
@@ -33,6 +33,17 @@ public class QuestionB {
 		return countOdd <= 1;
 	}
 	
+	public static int getCharNumber(Character c) {
+		int a = Character.getNumericValue('a');
+		int z = Character.getNumericValue('z');
+		
+		int val = Character.getNumericValue(c);
+		if (a <= val && val <= z) {
+			return val - a;
+		}
+		return -1;
+	}
+	
 	public static void main(String[] args) {
 		String pali = "Ratzs live on no evil starz";
 		System.out.println(isPermutationOfPalindrome(pali));
