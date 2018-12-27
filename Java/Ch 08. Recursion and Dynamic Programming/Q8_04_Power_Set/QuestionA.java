503b32b2bcaabdb52fc63fde4891cff6a52a766c
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 17:09:57 -0500

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_04_Power_Set/QuestionA.java
@@ -18,16 +18,16 @@ P(2) = {}, {a1}, {a2}, {a1, a2}
 P(3) = {}, {a1}, {a2}, {a3}, {a1, a2}, {a1, a3}, {a2, a3}, {a1, a2, a3}
 The difference between these solutions is that P (2) is missing all the subsets containing a3
 P(3) - P(2) = {a3}, {a1, a3}, {a2, a3}, {a1, a2, a3}
-How can we use P (2) to create P(3)? We can simply clone the subsets in P(2) and add a3 to them:
+How can we use P(2) to create P(3)? We can simply clone the subsets in P(2) and add a3 to them:
 
 P(2)  = {}, {a1}, {a2}, {a1, a2}
 P(2) + a3 = {a3}, {a1, a3}, {a2 , a3}, {a1, a2, a 3}
 
-When merged together, the lines above make P (3).
+When merged together, the lines above make P(3).
 
 Case:n > 0
-Generating P (n) for the general case is just a simple generalization of the above steps. We compute
-P (n-1), clone the results, and then add a n to each of these cloned sets.
+Generating P(n) for the general case is just a simple generalization of the above steps. We compute
+P(n-1), clone the results, and then add a n to each of these cloned sets.
 
 This solution will be O(n * 2 raisedTo n) in time and space, which is the best we can do. For a slight optimization, we
 could also implement this algorithm iteratively.
