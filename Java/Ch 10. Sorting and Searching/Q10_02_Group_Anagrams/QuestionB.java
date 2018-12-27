a310a58057aa806f6bc6a5ce1445b95ed759bdfa
comeon.getit <adityakapre2009@gmail.com>
2018-12-26 23:52:49 -0500

    Update QuestionB.java

--- Java/Ch 10. Sorting and Searching/Q10_02_Group_Anagrams/QuestionB.java
+++ Java/Ch 10. Sorting and Searching/Q10_02_Group_Anagrams/QuestionB.java
@@ -1,3 +1,26 @@
+/*
+This problem asks us to group the strings in an array such that the anagrams appear next to each other.
+Note that no specific ordering of the words is required, other than this.
+We need a quick and easy way of determining if two strings are anagrams of each other. What defines if two
+words are anagrams of each other? Well, anagrams are words that have the same characters but in different
+orders. It follows then that if we can put the characters in the same order, we can easily check if the new
+words are identical.
+One way to do this is to just apply any standard sorting algorithm, like merge sort or quick sort, and modify
+the comparator. This comparator will be used to indicate that two strings which are anagrams of each other
+are equivalent.
+What's the easiest way of checking if two words are anagrams? We could count the occurrences of the
+distinct characters in each string and return true if they match. Or, we could just sort the string. After all,
+two words which are anagrams will look the same once they're sorted.
+
+This algorithm will take O(n log(n)) time.
+
+This may be the best we can do for a general sorting algorithm, but we don't actually need to fully sort the
+array. We only need to group the strings in the array by anagram.
+We can do this by using a hash table which maps from the sorted version of a word to a list of its anagrams.
+So, for example, acre will map to the list {acre, race, care}. Once we've grouped all the words into
+these lists by anagram, we can then put them back into the array.
+
+*/
 package Q10_02_Group_Anagrams;
 
 import java.util.ArrayList;
