3296177d1bbb297a5cb1c9955b5b1dbc58212bf4
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 15:06:40 -0500

    Update AnimalQueue.java

--- Java/Ch 03. Stacks and Queues/Q3_06_Animal_Shelter/AnimalQueue.java
+++ Java/Ch 03. Stacks and Queues/Q3_06_Animal_Shelter/AnimalQueue.java
@@ -83,3 +83,25 @@ public class AnimalQueue {
 		return cats.peek();
 	}
 }
+
+abstract class Animal {
+	private int order; 
+	protected String name;
+	public Animal(String n) {
+		name = n;
+	}
+	
+	public abstract String name();
+	
+	public void setOrder(int ord) {
+		order = ord;
+	}
+	
+	public int getOrder() {
+		return order;
+	}
+	
+	public boolean isOlderThan(Animal a) {
+		return this.order < a.getOrder();
+	}
+}
