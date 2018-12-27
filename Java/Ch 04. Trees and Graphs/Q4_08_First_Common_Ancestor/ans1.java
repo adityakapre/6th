cfcd1460eb855e8d8a993bf451c0051dedad449d
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 16:11:35 -0500

    Update ans1.java

--- Java/Ch 04. Trees and Graphs/Q4_08_First_Common_Ancestor/ans1.java
+++ Java/Ch 04. Trees and Graphs/Q4_08_First_Common_Ancestor/ans1.java
@@ -3,6 +3,7 @@ If this were a binary search tree, we could modify the find operation for the tw
 paths diverge. Unfortunately, this is not a binary search tree, so we must try other approaches.
 Let's assume we're looking for the common ancestor of nodes p and q. One question to ask here is if each
 node in our tree has a link to its parents.
+
 Solution #1: With Links to Parents
 If each node has a link to its parent, we could trace p and q's paths up until they intersect. This is essentially
 the same problem as question 2.7 which find the intersection of two linked lists. The "linked list" in this case
