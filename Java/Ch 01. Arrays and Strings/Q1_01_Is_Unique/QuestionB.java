896ab3ddf481ac22c68c7ad6b7c16611ba28cc7e
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 10:53:25 -0500

    Update QuestionB.java

--- Java/Ch 01. Arrays and Strings/Q1_01_Is_Unique/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_01_Is_Unique/QuestionB.java
@@ -1,3 +1,18 @@
+/*
+We can reduce our space usage by a factor of eight by using a bit vector. We will assume, in the below code,
+that the string only uses the lowercase letters a through z. This will allow us to use just a single int
+
+If we can't use additional data structures, we can do the following:
+1. Compare every character of the string to every other character of the string. This will take 0(n sq.) time
+and 0(1) space.
+2. If we are allowed to modify the input string, we could sort the string in O(nlog(n)) time and then
+linearly check the string for neighboring characters that are identical. Careful, though: many sorting
+algorithms take up extra space.
+These solutions are not as optimal in some respects, but might be better depending on the constraints of
+the problem.
+
+checker will be 32 bits (4 bytes)
+*/
 package Q1_01_Is_Unique;
 
 public class QuestionB {
@@ -9,7 +24,7 @@ public class QuestionB {
 		}
 		int checker = 0;
 		for (int i = 0; i < str.length(); i++) {
-			int val = str.charAt(i) - 'a';
+			int val = str.charAt(i) - 'a';		//acsii value
 			if ((checker & (1 << val)) > 0) return false;
 			checker |= (1 << val);
 		}
