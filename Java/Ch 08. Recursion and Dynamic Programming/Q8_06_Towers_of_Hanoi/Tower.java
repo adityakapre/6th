4d2496f0a6f82cbf5f207e1eacdb03bd191d385e
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 17:26:33 -0500

    Update Tower.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_06_Towers_of_Hanoi/Tower.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_06_Towers_of_Hanoi/Tower.java
@@ -89,3 +89,25 @@ public class Tower {
 		buffer.moveDisks(quantity - 1, destination, this);
 	}
 }
+
+class Question {
+	public static void main(String[] args) {
+		Tower source = new Tower();
+		Tower destination = new Tower();
+		Tower buffer = new Tower();
+		
+		source.name = "s";
+		destination.name = "d";
+		buffer.name = "b";
+		
+		/* Load up tower */
+		int numberOfDisks = 5;
+		for (int disk = numberOfDisks - 1; disk >= 0; disk--) {
+			source.add(disk);
+		}
+		
+		source.print();
+		source.moveDisks(numberOfDisks, destination, buffer);
+		destination.print();
+	}
+}
