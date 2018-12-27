27c12f1684653a0ec186164e318cd11dcf302a70
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 15:03:29 -0500

    Update Question.java

--- Java/Ch 03. Stacks and Queues/Q3_05_Sort_Stack/Question.java
+++ Java/Ch 03. Stacks and Queues/Q3_05_Sort_Stack/Question.java
@@ -1,7 +1,7 @@
 /*
 One approach is to implement a rudimentary sorting algorithm. We search through the entire stack to find
 the minimum element and then push that onto a new stack. Then, we find the new minimum element
-and push that. This will actually require a total of three stacks: s 1 is the original stack, s2 is the final sorted
+and push that. This will actually require a total of three stacks: s1 is the original stack, s2 is the final sorted
 stack, and s3 acts as a buffer during our searching of sl. To search sl for each minimum, we need to pop
 elements from sl and push them onto the buffer, s3.
 Unfortunately, this requires two additional stacks, and we can only use one. Can we do better? Yes.
@@ -19,17 +19,17 @@ s2 : (top)
      3
      1
 
-When we pop 5 from s 1, we need to find the right place in s2 to insert this number. In this case, the correct
+When we pop 5 from s1, we need to find the right place in s2 to insert this number. In this case, the correct
 place is on s2 just above 3. How do we get it there? We can do this by popping 5 from sl and holding it
-in a temporary variable. Then, we move 12 and 8 over to s 1 (by popping them from s2 and pushing them
+in a temporary variable. Then, we move 12 and 8 over to s1 (by popping them from s2 and pushing them
 onto sl) and then push 5 onto s2.
 Note that 8 and 12 are still in sl-and that's okay! We just repeat the same steps for those two numbers as
 we did for 5, each time popping off the top of sl and putting it into the "right place" on s2. (Of course, since
-8 and 12 were moved from s2 to s 1 precisely because they were larger than 5, the "right place" for these
+8 and 12 were moved from s2 to s1 precisely because they were larger than 5, the "right place" for these
 elements will be right on top of 5. We won't need to muck around with s2's other elements, and the inside
 of the below while loop will not be run when tmp is 8 or 12.)
 
-This algorithm is O ( N2 ) time and O ( N) space.
+This algorithm is O (N sq.) time and O (N) space.
 If we were allowed to use unlimited stacks, we could implement a modified quicksort or mergesort.
 With the mergesort solution, we would create two extra stacks and divide the stack into two parts. We
 would recursively sort each stack, and then merge them back together in sorted order into the original
