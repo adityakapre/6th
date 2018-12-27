b03f612a9be345aed636a785d98193fad63b3fec
comeon.getit <adityakapre2009@gmail.com>
2018-12-27 12:54:16 -0500

    Update Question.java

--- Java/Ch 02. Linked Lists/Q2_08_Loop_Detection/Question.java
+++ Java/Ch 02. Linked Lists/Q2_08_Loop_Detection/Question.java
@@ -28,32 +28,33 @@ So now we know the following facts:
 3. SlowRunner is K steps behind FastRunner.
 4. FastRunner is LOOP SIZE - K steps behind SlowRunner.
 5. FastRunner catches up to SlowRunner at a rate of 1 step per unit of time.
-So, when do they meet? Well, if FastRunner is LOOP SIZE K steps behind SlowRunner, and
-FastRunner catches up at a rate of 1 step per unit of time, then they meet after L OOP SIZE - K steps.
+So, when do they meet? Well, if FastRunner is LOOP_SIZE - K steps behind SlowRunner, and
+FastRunner catches up at a rate of 1 step per unit of time, then they meet after LOOP_SIZE - K steps.
 At this point, they will be K steps before the head of the loop. Let's call this point Collisions pot.
 
 Part 3: How Do You Find The Start of the Loop?
-We now know that CollisionSpot is K nodes before the start of the loop. Because K = mod (k, LOOP_
-SIZE) (or, in other words, k = K + M * LOOP SIZE, for any integer M), it is also correct to say that it is
+We now know that CollisionSpot is K nodes before the start of the loop. Because K = mod(k, LOOP_SIZE) 
+(or, in other words, k = K + M * LOOP SIZE, for any integer M), it is also correct to say that it is
 k nodes from the loop start. For example, if node N is 2 nodes into a 5 node loop, it is also correct to say that
 it is 7, 12, or even 397 nodes into the loop.
 Therefore, both CollisionSpot and LinkedlistHead are k nodes from the start of the loop.
 
 Now, if we keep one pointer at CollisionSpot and move the other one to LinkedListHead, they will
-each be k nodes from LoopSta rt. Moving the two pointers at the same speed will cause them to collide
+each be k nodes from LoopStart. Moving the two pointers at the same speed will cause them to collide
 again-this time after k steps, at which point they will both be at LoopStart. All we have to do is return
 this node.
 
 Part 4: Putting It All Together
 To summarize, we move FastPointer twice as fast as SlowPointer. When SlowPointer enters
 the loop, after k nodes, FastPointer is k nodes into the loop. This means that FastPointer and
-SlowPointer are LOOP _SIZE - k nodes away from each other.
+SlowPointer are LOOP_SIZE - k nodes away from each other.
 Next, if FastPointer moves two nodes for each node that SlowPointer moves, they move one node
-closer to each other on each turn. Therefore, they will meet after LOOP _SIZE - k turns. Both will be k
+closer to each other on each turn. Therefore, they will meet after LOOP_SIZE - k turns. Both will be k
 nodes from the front of the loop.
 The head of the linked list is also k nodes from the front of the loop. So, if we keep one pointer where it is,
 and move the other pointer to the head of the linked list, then they will meet at the front of the loop.
 Our algorithm is derived directly from parts 1, 2 and 3.
+
 1. Create two pointers, FastPointer and SlowPointer.
 2. Move FastPointer at a rate of 2 steps and SlowPointer at a rate of 1 step.
 3. When they collide, move SlowPointer to LinkedListHead. Keep FastPointer where it is.
