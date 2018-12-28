d0fbb66e9a245b41eb786c904e1027275c5ea47a
comeon.getit <adityakapre2009@gmail.com>
2018-12-28 13:18:08 -0500

    Update and rename ReverseLinkedList to ReverseLinkedList.java

--- Java/Ch 02. Linked Lists/ReverseLinkedList.java
+++ Java/Ch 02. Linked Lists/ReverseLinkedList.java
@@ -1,3 +1,4 @@
+/*
 Q. Code writing: write a function to reverse a linked list 
 
 Iterative Method
@@ -5,6 +6,7 @@ Iterative Method
 prev -> current -> next
 
 Iterate through the linked list. In loop, change next to prev, prev to current and current to next.          
+*/
 
 static void reverse(Node head) {
     if(null == head) return;
@@ -20,11 +22,13 @@ static void reverse(Node head) {
     head = prev; //head is prev since current is null here - while loop condition
 }
 
+/*
 2. Recursive Method:
    	1) Divide list in 2 parts - first node & rest list 
    	2) Recursively Call reverse for rest  
 3) Link rest to first. 
 4) Fix head
+*/
 
 void recursiveReverse(Node head) {
     Node first; Node rest;
