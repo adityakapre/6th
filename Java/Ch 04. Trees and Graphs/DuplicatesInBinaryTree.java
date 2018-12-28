5c83235278a8b840e56ccda37f9e4707df7a239f
comeon.getit <adityakapre2009@gmail.com>
2018-12-28 13:19:01 -0500

    Update and rename DuplicatesInBinaryTree to DuplicatesInBinaryTree.java

--- Java/Ch 04. Trees and Graphs/DuplicatesInBinaryTree.java
+++ Java/Ch 04. Trees and Graphs/DuplicatesInBinaryTree.java
@@ -1,8 +1,10 @@
+/*
 Q. Count duplicates in a binary tree  
 
-/*Approach is to do inoorder traversal and check if each node matches its previous node
-* use inorder traversal (LCR) since it goes sequentially in increasing order
+Approach is to do inoorder traversal and check if each node matches its previous node
+use inorder traversal (LCR) since it goes sequentially in increasing order
 */
+
 int duplicates=0;    // global variable to count duplicates
 public Node countDuplicatesLCR(Node root, Node prev) {
  	 if(root==null) 
