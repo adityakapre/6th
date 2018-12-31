117f3eb8a5c613338a138f46debffaf7dc3c5a91
comeon.getit <adityakapre2009@gmail.com>
2018-12-31 12:19:22 -0500

    Update Question.java

--- Java/Ch 10. Sorting and Searching/Q10_08_Find_Duplicates/Question.java
+++ Java/Ch 10. Sorting and Searching/Q10_08_Find_Duplicates/Question.java
@@ -4,6 +4,10 @@ We have 4 kilobytes of memory which means we can address up to 8 * 4 * 210 bits.
 bits is greater than 32000. We can create a bit vector with 32000 bits, where each bit represents one integer.
 Using this bit vector, we can then iterate through the array, flagging each element v by setting bit v to 1.
 When we come across a duplicate element, we print it.
+
+Note that while this isn't an especially difficult problem, it's important to implement this cleanly. This is why
+we defined our own bit vector class to hold a large bit vector. If our interviewer lets us (she may or may not),
+we could have of course used Java's built in BitSet class.
 */
 package Q10_08_Find_Duplicates;
 
