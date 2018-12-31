db8a08885af686b85b48e244eda0f8fccab4a20e
comeon.getit <adityakapre2009@gmail.com>
2018-12-30 23:27:25 -0500

    Update QuestionB.java

--- Java/Ch 10. Sorting and Searching/Q10_07_Missing_Int/QuestionB.java
+++ Java/Ch 10. Sorting and Searching/Q10_07_Missing_Int/QuestionB.java
@@ -1,3 +1,47 @@
+/*
+Follow Up: What if we have only 10 MB memory?
+It's possible to find a missing integer with two passes of the data set. We can divide up the integers into
+blocks of some size (we'll discuss how to decide on a size later). Let's just assume that we divide up the
+integers into blocks of 1000. So, block O represents the numbers O through 999, block 1 represents numbers
+1000 - 1999, and so on.
+Since all the values are distinct, we know how many values we should find in each block. So, we search
+through the file and count how many values are between O and 999, how many are between 1000 and
+1999, and so on. If we count only 999 values in a particular range, then we know that a missing int must be
+in that range.
+In the second pass, we'll actually look for which number in that range is missing. We use the bit vector
+approach from the first part of this problem. We can ignore any number outside of this specific range.
+The question, now, is what is the appropriate block size? Let's define some variables as follows:
+
+-> Let rangeSize be the size of the ranges that each block in the first pass represents.
+-> Let arrayS1ze represent the number of blocks in the first pass. Note that arraySize = 2 rasiedTo 31 / rangesize
+since there are 2 raisedTo 31 non-negative integers.
+
+We need to select a value for rangeSize such that the memory from the first pass (the array) and the
+second pass (the bit vector) fit.
+
+First Pass: The Array
+The array in the first pass can fit in 10 megabytes, or roughly 2 rasiedTo 23 bytes, of memory. Since each element in the
+array is an int, and an int is 4 bytes, we can hold an array of at most about 2 rasiedTo 21 elements. So, we can deduce
+the following:
+
+arraySize = 2 rasiedTo 31/rangeSize <= 2 rasiedTo 21
+rangeS1ze >= 2 rasiedTo 31/2 rasiedTo 21
+rangeSize >= 2 rasiedTo 10
+
+Second Pass: The Bit Vector
+We need to have enough space to store rangeSize bits. Since we can fit 2 rasiedTo 23 bytes in memory, we can fit
+2 rasiedTo 26 bits in memory. Therefore, we can conclude the following:
+2 rasiedTo 11 <= rangeSize <= 2 rasiedTo 26
+
+These conditions give us a good amount of"wiggle room;' but the nearer to the middle that we pick, the
+less memory will be used at any given time.
+The below code provides one implementation for this algorithm.
+
+What if, as a follow up question, you are asked to solve the problem with even less memory? In this case, we
+can do repeated passes using the approach from the first step. We'd first check to see how many integers
+are found within each sequence of a million elements. Then, in the second pass, we'd check how many integers
+are found in each sequence of a thousand elements. Finally, in the third pass, we'd apply the bit vector.
+*/
 package Q10_07_Missing_Int;
 
 import java.io.*;
