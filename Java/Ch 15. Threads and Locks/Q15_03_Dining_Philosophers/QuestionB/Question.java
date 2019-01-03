34f228c10e299bf9fdb052cc97ac1c09d4f5ca3a
comeon.getit <adityakapre2009@gmail.com>
2019-01-03 15:49:42 -0500

    Update Question.java

--- Java/Ch 15. Threads and Locks/Q15_03_Dining_Philosophers/QuestionB/Question.java
+++ Java/Ch 15. Threads and Locks/Q15_03_Dining_Philosophers/QuestionB/Question.java
@@ -1,3 +1,23 @@
+/*
+Solution #1: All or Nothing
+To prevent deadlocks, we can implement a strategy where a philosopher will put down his left chopstick if
+he is unable to obtain the right one.
+
+In the above code, we need to be sure to release the left chopstick if we can't pick up the right one-and to
+not call putDown() on the chopsticks if we never had them in the first place.
+One issue with this is that if all the philosophers were perfectly synchronized, they could simultaneously
+pick up their left chopstick, be unable to pick up the right one, and then put back down the left one-only
+to have the process repeated again.
+
+Solution #2: Prioritized Chopsticks
+Alternatively, we can label the chopsticks with a number from 0 to N - 1. Each philosopher attempts to
+pick up the lower numbered chopstick first. This essentially means that each philosopher goes for the left
+chopstick before right one (assuming that's the way you labeled it), except for the last philosopher who
+does this in reverse. This will break the cycle.
+
+With this solution, a philosopher can never hold the larger chopstick without holding the smaller one. This
+prevents the ability to have a cycle, since a cycle means that a higher chopstick would "point"to a lower one.
+*/
 package Q15_03_Dining_Philosophers.QuestionB;
 
 public class Question {
