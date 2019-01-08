a5d2be59812f1de78a8e801dd30afd9f3d970830
comeon.getit <adityakapre2009@gmail.com>
2019-01-08 10:28:46 -0500

    Update Question.java

--- Java/Ch 04. Trees and Graphs/Q4_07_Build_Order/DFS/Question.java
+++ Java/Ch 04. Trees and Graphs/Q4_07_Build_Order/DFS/Question.java
@@ -1,3 +1,9 @@
+/*
+We start by finding nodes that can be built last (no nodes depend on them) and add such nodes to stack
+So stack starts by pushing nodes that can be built last.
+therefore, Stack top contains nodes that can be built first
+*/
+
 package Q4_07_Build_Order.DFS;
 
 import java.util.ArrayList;
