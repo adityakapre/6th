504093f6a7a231400a393020795e403e31693a73
comeon.getit <adityakapre2009@gmail.com>
2019-01-08 11:35:38 -0500

    Update Question.java

--- Java/Ch 04. Trees and Graphs/Q4_09_BST_Sequences/Question.java
+++ Java/Ch 04. Trees and Graphs/Q4_09_BST_Sequences/Question.java
@@ -21,8 +21,53 @@ Once the 50 is inserted, all items less than 50 will be routed to the left and a
 routed to the right. The 60 or the 20 could be inserted first, and it wouldn't matter.
 Let's think about this problem recursively. If we had all arrays that could have created the subtree rooted
 at 20 (call this arraySet20), and all arrays that could have created the subtree rooted at 60 (call this
-a rray5et60), how would that give us the full answer? We could just"weave" each array from a rray5et20
+arraySet60), how would that give us the full answer? We could just"weave" each array from arraySet20
 with each array from arraySet60-and then prepend each array with a 50.
+
+Here's what we mean by weaving. We are merging two arrays in all possible ways, while keeping the
+elements within each array in the same relative order.
+arrayl: {l, 2}
+array2: {3, 4}
+weaved: {l, 2, 3, 4}, {l, 3, 2, 4}, {1, 3, 4, 2}, {3, 1, 2, 4}, {3, 1, 4, 2}, {3, 4, 1, 2}
+
+Note that, as long as there aren't any duplicates in the original array sets, we won't have to worry that
+weaving will create duplicates.
+The last piece to talk about here is how the weaving works . Let's think recursively about how to weave {1,2,3}
+and {4,5,6}. What are the subproblems?
+Prepend a l to all weaves of {2,3} and {4,5,6}.
+Prepend a 4 to all weaves of {l,2,3} and {5,6}.
+To implement this, we'll store each as linked lists. This will make it easy to add and remove elements. When
+we recurse, we'll push the prefixed elements down the recursion . When first or second are empty, we
+add the remainder to prefix and store the result.
+
+It works something like this:
+weave(first, second, prefix):
+	weave({l, 2}, {3, 4}, {})
+		weave({2}, {3, 4}, {1})
+			weave({}, {3, 4}, {l, 2})
+				{1, 2, 3, 4}
+			weave({2}, {4}, {1, 3})
+				weave({}, {4}, {l, 3, 2})
+					{l, 3, 2, 4}
+				weave({2}, {}, {l, 3, 4})
+					{l, 3, 4, 2}
+			weave({l, 2}, {4}, {3})
+				weave({2}, {4}, {3, 1})
+					weave({}, {4}, {3, 1, 2})
+						{3, 1, 2, 4}
+					weave({2}, {}, {3, 1, 4})
+						{3, 1, 4, 2}
+				weave({l, 2}, {}, {3, 4})
+					{3, 4, 1, 2}
+Now, let's think through the implementation of removing, say, 1 from { 1, 2} and recursing. We need to be
+careful about modifying this list, since a later recursive call (e .g., weave( {1, 2}, { 4}, {3})) might need
+the 1 still in {1, 2}.
+We could clone the list when we recurse, so that we only modify the recursive calls. Or, we could modify the
+list but then "revert"the changes after we're done with recursing.
+We've chosen to implement it the latter way. Since we're keeping the same reference to first, second, and
+prefix the entire way down the recursive call stack, then we'll need to clone prefix just before we store
+the complete result.
+
 */
 package Q4_09_BST_Sequences;
 
