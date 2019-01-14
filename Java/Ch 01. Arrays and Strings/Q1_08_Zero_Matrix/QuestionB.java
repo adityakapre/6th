5c93699d41f34335f723fb7ea919648d19fb9bb9
comeon.getit <adityakapre2009@gmail.com>
2019-01-14 11:32:01 -0500

    Update QuestionB.java

--- Java/Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/QuestionB.java
+++ Java/Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/QuestionB.java
@@ -6,10 +6,10 @@ column as a replacement for the column array. This works as follows:
 
 1. Check if the first row and first column have any zeros, and set variables rowHasZero and
 columnHasZero. (We'll nullify the first row and first column later, if necessary.)
-2. Iterate through the rest of the matrix, setting matrix[i][0] and matrix[0][j] to zero whenever
-there's a zero in matrix[i][j].
-3. Iterate through rest of matrix, nullifying row i if there's a zero in matrix[i][0].
-4. Iterate through rest of matrix, nullifying column j if there's a zero in matrix[0][j].
+2. Iterate through the rest of the matrix, setting matrix[r][0] and matrix[0][c] to zero whenever
+there's a zero in matrix[r][c].
+3. Iterate through rest of matrix, nullifying row r if there's a zero in matrix[r][0].
+4. Iterate through rest of matrix, nullifying column c if there's a zero in matrix[0][c].
 5. Nullify the first row and first column, if necessary (based on values from Step 1 ).
 
 This code has a lot of "do this for the rows, then the equivalent action for the column:' In an interview, you
@@ -22,59 +22,48 @@ package Q1_08_Zero_Matrix;
 import CtCILibrary.AssortedMethods;
 
 public class QuestionB {
-	public static void nullifyRow(int[][] matrix, int row) {
-		for (int j = 0; j < matrix[0].length; j++) {
-			matrix[row][j] = 0;
-		}		
-	}
-
-	public static void nullifyColumn(int[][] matrix, int col) {
-		for (int i = 0; i < matrix.length; i++) {
-			matrix[i][col] = 0;
-		}		
-	}		
 	
 	public static void setZeros(int[][] matrix) {
 		boolean rowHasZero = false;
 		boolean colHasZero = false;		
 		
 		// Check if first row has a zero
-		for (int j = 0; j < matrix[0].length; j++) {
-			if (matrix[0][j] == 0) {
+		for (int c = 0; c < matrix[0].length; c++) {
+			if (matrix[0][c] == 0) {
 				rowHasZero = true;
 				break;
 			}
 		}		
 		
 		// Check if first column has a zero
-		for (int i = 0; i < matrix.length; i++) {
-			if (matrix[i][0] == 0) {
+		for (int r = 0; r < matrix.length; r++) {
+			if (matrix[r][0] == 0) {
 				colHasZero = true;
 				break;
 			}
 		}		
 		
 		// Check for zeros in the rest of the array
-		for (int i = 1; i < matrix.length; i++) {
-			for (int j = 1; j < matrix[0].length;j++) {
-				if (matrix[i][j] == 0) {
-					matrix[i][0] = 0;
-					matrix[0][j] = 0;
+		for (int r = 1; r < matrix.length; r++) {
+			for (int c = 1; c < matrix[0].length; c++) {
+				if (matrix[r][c] == 0) {
+					matrix[r][0] = 0;
+					matrix[0][c] = 0;
 		 		}
 			}
 		}		
 		
 		// Nullify rows based on values in first column
-		for (int i = 1; i < matrix.length; i++) {
-			if (matrix[i][0] == 0) {
-				nullifyRow(matrix, i);
+		for (int r = 1; r < matrix.length; r++) {
+			if (matrix[r][0] == 0) {
+				nullifyRow(matrix, r);
 			}
 		}		
 		
 		// Nullify columns based on values in first row
-		for (int j = 1; j < matrix[0].length; j++) {
-			if (matrix[0][j] == 0) {
-				nullifyColumn(matrix, j);
+		for (int c = 1; c < matrix[0].length; c++) {
+			if (matrix[0][c] == 0) {
+				nullifyColumn(matrix, c);
 			}
 		}	
 		
@@ -87,7 +76,19 @@ public class QuestionB {
 		if (colHasZero) {
 			nullifyColumn(matrix, 0);
 		}
-	}	
+	}
+	
+	public static void nullifyRow(int[][] matrix, int row) {
+		for (int c = 0; c < matrix[0].length; c++) {
+			matrix[row][c] = 0;
+		}		
+	}
+
+	public static void nullifyColumn(int[][] matrix, int col) {
+		for (int r = 0; r < matrix.length; r++) {
+			matrix[r][col] = 0;
+		}		
+	}		
 	
 	public static boolean matricesAreEqual(int[][] m1, int[][] m2) {
 		if (m1.length != m2.length || m1[0].length != m2[0].length) {
