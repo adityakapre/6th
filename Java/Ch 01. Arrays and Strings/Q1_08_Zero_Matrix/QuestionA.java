c0c1fd66ef6be2d936d21a4178a993358263b21f
comeon.getit <adityakapre2009@gmail.com>
2019-01-14 11:28:31 -0500

    Update QuestionA.java

--- Java/Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/QuestionA.java
+++ Java/Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/QuestionA.java
@@ -24,11 +24,11 @@ public class QuestionA {
 		boolean[] column = new boolean[matrix[0].length];
 
 		// Store the row and column index with value 0
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length;j++) {
-				if (matrix[i][j] == 0) {
-					row[i] = true; 
-					column[j] = true;
+		for (int r = 0; r < matrix.length; r++) {
+			for (int c = 0; c < matrix[0].length;c++) {
+				if (matrix[r][c] == 0) {
+					row[r] = true; 
+					column[c] = true;
 		 		}
 			}
 		}
