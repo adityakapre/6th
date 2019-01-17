3f458cc06130a01ab53dd08fe187cb4da82fa690
comeon.getit <adityakapre2009@gmail.com>
2019-01-17 16:29:13 -0500

    Update PriorityQueue.java

--- Java/Ch 03. Stacks and Queues/PriorityQueue.java
+++ Java/Ch 03. Stacks and Queues/PriorityQueue.java
@@ -22,7 +22,7 @@ so parent(8) = (int) 8/2 = 4 = i/2
 so parent(9) = (int) 9/2 = 4 = i/2
 
 child(4) = 2*4 = 8 = 2i
-child(4) = 2*4 +1 = 9 = 2i+1
+child(4) = 2*4 + 1 = 9 = 2i+1
 
 So for simplicity, below program starts with index 1 rather than index 0
 
