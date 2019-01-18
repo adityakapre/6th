1e8a7144843d0026a22bbebb8f882334b1428c04
comeon.getit <adityakapre2009@gmail.com>
2019-01-18 17:13:55 -0500

    Update QuestionB.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_11_Coins/QuestionB.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_11_Coins/QuestionB.java
@@ -54,6 +54,12 @@ public class QuestionB {
 	/*
 	Iterative  - 2D array
 	https://www.youtube.com/watch?v=_fgjrs570YE
+	
+		0 1 2 3 4 5 ->j  total=5
+	      1 1 1 1 1 1 1
+	      2 1 1 2 2 3 3
+	      3 1 1 2 3 4 5
+	      i
 	*/
 	public int numberOfSolutions(int coins[], int total){
 		int temp[][] = new int[coins.length+1][total+1];
