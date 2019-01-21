322a7a3ac6fcf2d87bbdfb2d9dc8b09701aded76
comeon.getit <adityakapre2009@gmail.com>
2019-01-21 09:09:10 -0500

    Update QuestionA.java

--- Java/Ch 01. Arrays and Strings/Q1_05_One_Away/QuestionA.java
+++ Java/Ch 01. Arrays and Strings/Q1_05_One_Away/QuestionA.java
@@ -55,12 +55,13 @@ public class QuestionA {
 		int index2 = 0;
 		while (index2 < s2.length() && index1 < s1.length()) {
 			if (s1.charAt(index1) != s2.charAt(index2)) {
-				if (index1 != index2) {
-					return false;
-				}	
 				//since s2 larger in length, we progress its ptr only once
 				//when chars are not same
-				index2++; 
+				if (index1 == index2) {
+					index2++; 
+				} else {
+					return false;	
+				}
 			} else {
 				index1++;
 				index2++;
