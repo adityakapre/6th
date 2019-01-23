a997e33e8f1c19586379604e61c2e74ed497f9af
comeon.getit <adityakapre2009@gmail.com>
2019-01-23 11:07:29 -0500

    Update FixedMultiStack.java

--- Java/Ch 03. Stacks and Queues/Q3_01_Three_in_One/FixedMultiStack.java
+++ Java/Ch 03. Stacks and Queues/Q3_01_Three_in_One/FixedMultiStack.java
@@ -116,10 +116,11 @@ class FullStackException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public FullStackException(){
-        super();
-    }
-
-    public FullStackException(String message){
-        super(message);
-    }
+        	super();
+    	}
+	
+	//remember this to call super(message)
+    	public FullStackException(String message){
+        	super(message);
+    	}
 }
