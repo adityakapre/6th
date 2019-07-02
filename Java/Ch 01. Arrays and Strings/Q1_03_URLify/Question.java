0d88294293fec0aa548acf40a1b7aa8dbacc7e78
comeon.getit <adityakapre2009@gmail.com>
2019-07-02 10:58:26 -0400

    Update Question.java

--- Java/Ch 01. Arrays and Strings/Q1_03_URLify/Question.java
+++ Java/Ch 01. Arrays and Strings/Q1_03_URLify/Question.java
@@ -1,4 +1,15 @@
 /*
+Q :
+Write a method to replace all spaces in a string with'%20'. You may assume that
+the string has sufficient space at the end of the string to hold the additional
+characters, and that you are given the "true" length of the string. (Note: if implementing
+in Java, please use a character array so that you can perform this operation
+in place.)
+EXAMPLE
+Input: "Mr John Smith
+Output: "Mr%20Dohn%20Smith"
+
+A:
 A common approach in string manipulation problems is to edit the string starting from the end and working
 backwards. This is useful because we have an extra buffer at the end, which allows us to change characters
 without worrying about what we're overwriting.
