1421d0057ac9cef79d4421a006b887af82b35151
comeon.getit <adityakapre2009@gmail.com>
2019-07-08 14:59:35 -0400

    Update QuestionA.java

--- Java/Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/QuestionA.java
+++ Java/Ch 01. Arrays and Strings/Q1_08_Zero_Matrix/QuestionA.java
@@ -1,4 +1,9 @@
 /*
+Q:
+Write an algorithm such that if an element in an MxN matrix is 0, its entire row
+and column are set to 0.
+
+A:
 At first glance, this problem seems easy: just iterate through the matrix and every time we see a cell with
 value zero, set its row and column to 0. There's one problem with that solution though: when we come
 across other cells in that row or column, we'll see the zeros and change their row and column to zero. Pretty
