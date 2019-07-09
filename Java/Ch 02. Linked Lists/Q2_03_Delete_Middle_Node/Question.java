c3fa2d3d1e7a4809c0b45f251e577809fb6e1e9d
comeon.getit <adityakapre2009@gmail.com>
2019-07-09 12:17:31 -0400

    Update Question.java

--- Java/Ch 02. Linked Lists/Q2_03_Delete_Middle_Node/Question.java
+++ Java/Ch 02. Linked Lists/Q2_03_Delete_Middle_Node/Question.java
@@ -1,4 +1,13 @@
 /*
+Q:
+Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but
+the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+that node.
+EXAMPLE
+lnput:the node c from the linked list a->b->c->d->e->f
+Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+
+A:
 In this problem, you are not given access to the head of the linked list. You only have access to that node.
 The solution is simply to copy the data from the next node over to the current node, and then to delete the
 next node.
