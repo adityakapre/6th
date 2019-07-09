79c80453336092942c449d844e99e7e872bd47e0
comeon.getit <adityakapre2009@gmail.com>
2019-07-09 15:23:13 -0400

    Update and rename Java/Ch 02. Linked Lists/Q2_05_Sum_Lists/05_Sum_Lists.java to Java/Ch 02. Linked Lists/05_Sum_Lists.java

--- Java/Ch 02. Linked Lists/05_Sum_Lists.java
+++ Java/Ch 02. Linked Lists/05_Sum_Lists.java
@@ -207,3 +207,12 @@ public class QuestionB {
 		System.out.print(l1 + " + " + l2 + " = " + (l1 + l2));		
 	}
 }
+
+package Q2_05_Sum_Lists;
+
+import CtCILibrary.LinkedListNode;
+
+public class PartialSum {
+	public LinkedListNode sum = null;
+	public int carry = 0;
+}
