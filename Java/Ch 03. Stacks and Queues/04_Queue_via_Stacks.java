7ed08312b277849bed2ded7b37e186e52647b4ec
comeon.getit <adityakapre2009@gmail.com>
2019-07-10 12:29:47 -0400

    Update 04_Queue_via_Stacks.java

--- Java/Ch 03. Stacks and Queues/04_Queue_via_Stacks.java
+++ Java/Ch 03. Stacks and Queues/04_Queue_via_Stacks.java
@@ -11,8 +11,8 @@ the peek/ pop operation, and then push everything back.
 This will work, but if two pop/ peeks are performed back-to-back, we're needlessly moving elements. We
 can implement a "lazy" approach where we let the elements sit in s2 until we absolutely must reverse the
 elements.
-In this approach, stackNewest has the newest elements on top and stackOldest has the oldest
-elements on top. When we dequeue an element, we want to remove the oldest element first, and so we
+****In this approach, stackNewest has the newest elements on top and stackOldest has the oldest elements on top.****
+When we dequeue an element, we want to remove the oldest element first, and so we
 dequeue from stackOldest. If stackOldest is empty, then we want to transfer all elements from
 stackNewest into this stack in reverse order. To insert an element, we push onto stackNewest, since it
 has the newest elements on top.
