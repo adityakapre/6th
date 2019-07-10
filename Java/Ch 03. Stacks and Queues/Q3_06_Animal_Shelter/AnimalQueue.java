7d635842a7bfe6ad76b5372a4652529026731e11
comeon.getit <adityakapre2009@gmail.com>
2019-07-10 16:30:04 -0400

    Update AnimalQueue.java

--- Java/Ch 03. Stacks and Queues/Q3_06_Animal_Shelter/AnimalQueue.java
+++ Java/Ch 03. Stacks and Queues/Q3_06_Animal_Shelter/AnimalQueue.java
@@ -1,4 +1,13 @@
 /*
+Q:
+Animal Shelter: An animal shelter, which holds only dogs and cats, operates on a strictly"first in, first
+out" basis. People must adopt either the "oldest" (based on arrival time) of all animals at the shelter,
+or they can select whether they would prefer a dog or a cat (and will receive the oldest animal of
+that type). They cannot select which specific animal they would like. Create the data structures to
+maintain this system and implement operations such as enqueue, dequeueAny, dequeueDog,
+and dequeueCat. You may use the built-in Linkedlist data structure.
+
+A:
 We could explore a variety of solutions to this problem. For instance, we could maintain a single queue.
 This would make dequeueAny easy, but dequeueDog and dequeueCat would require iteration through
 the queue to find the first dog or cat. This would increase the complexity of the solution and decrease the
