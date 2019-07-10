977978bdcfc9c445ed460a543939aa40321c4224
comeon.getit <adityakapre2009@gmail.com>
2019-07-10 12:07:21 -0400

    Update SetOfStacks.java

--- Java/Ch 03. Stacks and Queues/Q3_03_Stack_of_Plates/SetOfStacks.java
+++ Java/Ch 03. Stacks and Queues/Q3_03_Stack_of_Plates/SetOfStacks.java
@@ -1,4 +1,15 @@
 /*
+Q:
+Stack of Plates: Imagine a (literal) stack of plates. If the stack gets too high, it might topple.
+Therefore, in real life, we would likely start a new stack when the previous stack exceeds some
+threshold. Implement a data structure SetOfStacks that mimics this. SetOfStacks should be
+composed of several stacks and should create a new stack once the previous one exceeds capacity.
+SetOfStacks.push() and SetOfStacks.pop() should behave identically to a single stack
+(that is, pop() should return the same values as it would if there were just a single stack).
+FOLLOW UP
+Implement a function popAt(int index) which performs a pop operation on a specific substack.
+
+A:
 We know that push() should behave identically to a single stack, which means that we need push() to
 call push() on the last stack in the array of stacks. We have to be a bit careful here though: if the last stack
 is at capacity, we need to create a new stack. Our code should look something like this.
