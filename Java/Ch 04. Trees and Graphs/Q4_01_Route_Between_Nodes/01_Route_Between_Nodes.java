ce190a8be35583008ef4c7a72ad2e78e8f912e56
comeon.getit <adityakapre2009@gmail.com>
2019-07-10 16:59:57 -0400

    Update 01_Route_Between_Nodes.java

--- Java/Ch 04. Trees and Graphs/Q4_01_Route_Between_Nodes/01_Route_Between_Nodes.java
+++ Java/Ch 04. Trees and Graphs/Q4_01_Route_Between_Nodes/01_Route_Between_Nodes.java
@@ -18,88 +18,22 @@ package Q4_01_Route_Between_Nodes;
 
 import java.util.LinkedList;
 
-public class Question {
-	public enum State {
-		Unvisited, Visited, Visiting;
-	} 
-
-	public static boolean search(Graph g,Node start,Node end) {  
-		LinkedList<Node> q = new LinkedList<Node>();
-		for (Node u : g.getNodes()) {
-		    u.state = State.Unvisited;
-		}
-		start.state = State.Visiting;
-		q.add(start);
-		Node u;
-		while(!q.isEmpty()) {
-		    u = q.removeFirst();
-		    if (u != null) {
-			    for (Node v : u.getAdjacent()) {
-				if (v.state == State.Unvisited) {
-				    if (v == end) {
-					return true;
-				    } else {
-					v.state = State.Visiting;
-					q.add(v);
-				    }
-				}
-			    }
-			    //added to queue all adjacent nodes of u, so mark it visited
-			    u.state = State.Visited;
-		    }
-		}
-		return false;
-    }
-	
-	public static void main(String a[])
-	{
-		Graph g = createNewGraph();
-		Node[] n = g.getNodes();
-		Node start = n[3];
-		Node end = n[5];
-		System.out.println(search(g, start, end));
-	}
-	
-	public static Graph createNewGraph()
-	{
-		Graph g = new Graph();        
-		Node[] temp = new Node[6];
-
-		temp[0] = new Node("a", 3);
-		temp[1] = new Node("b", 0);
-		temp[2] = new Node("c", 0);
-		temp[3] = new Node("d", 1);
-		temp[4] = new Node("e", 1);
-		temp[5] = new Node("f", 0);
-
-		temp[0].addAdjacent(temp[1]);
-		temp[0].addAdjacent(temp[2]);
-		temp[0].addAdjacent(temp[3]);
-		temp[3].addAdjacent(temp[4]);
-		temp[4].addAdjacent(temp[5]);
-		for (int i = 0; i < 6; i++) {
-			g.addNode(temp[i]);
-		}
-		return g;
-	}
-}
-
 class Graph {
-	private Node[] vertices;
-	public static int MAX_VERTICES = 6;
-	public int count;
-	public Graph() {
-		vertices = new Node[MAX_VERTICES];
-		count = 0;
+    private Node[] vertices;
+    public static int MAX_VERTICES = 6;
+    public int count;
+    public Graph() {
+    	vertices = new Node[MAX_VERTICES];
+    	count = 0;
     }
 	
     public void addNode(Node x) {
-		if (count < vertices.length) {
-			vertices[count] = x;
-			count++;
-		} else {
-			System.out.print("Graph full");
-		}
+	if (count < vertices.length) {
+		vertices[count] = x;
+		count++;
+	} else {
+		System.out.print("Graph full");
+	}
     }
     
     public Node[] getNodes() {
@@ -132,4 +66,68 @@ class Node {
     public String getVertex() {
         return vertex;
     }
+}
+
+public class Question {
+	public enum State {
+		Unvisited, Visited, Visiting;
+	} 
+
+	public static boolean search(Graph g,Node start,Node end) {  
+		LinkedList<Node> q = new LinkedList<Node>();
+		for (Node u : g.getNodes()) {
+		    u.state = State.Unvisited;
+		}
+		start.state = State.Visiting;
+		q.add(start);
+		Node u;
+		while(!q.isEmpty()) {
+		    u = q.removeFirst();
+		    if (u != null) {
+			    for (Node v : u.getAdjacent()) {
+				if (v.state == State.Unvisited) {
+				    if (v == end) {
+					return true;
+				    } else {
+					v.state = State.Visiting;
+					q.add(v);
+				    }
+				}
+			    }
+			    //added to queue all adjacent nodes of u, so mark it visited
+			    u.state = State.Visited;
+		    }
+		}
+		return false;
+	}
+	
+	public static void main(String a[]) {
+		Graph g = createNewGraph();
+		Node[] n = g.getNodes();
+		Node start = n[3];
+		Node end = n[5];
+		System.out.println(search(g, start, end));
+	}
+	
+	public static Graph createNewGraph() {
+		Graph g = new Graph();        
+		Node[] temp = new Node[6];
+
+		temp[0] = new Node("a", 3);
+		temp[1] = new Node("b", 0);
+		temp[2] = new Node("c", 0);
+		temp[3] = new Node("d", 1);
+		temp[4] = new Node("e", 1);
+		temp[5] = new Node("f", 0);
+
+		temp[0].addAdjacent(temp[1]);
+		temp[0].addAdjacent(temp[2]);
+		temp[0].addAdjacent(temp[3]);
+		temp[3].addAdjacent(temp[4]);
+		temp[4].addAdjacent(temp[5]);
+		for (int i = 0; i < 6; i++) {
+			g.addNode(temp[i]);
+		}
+		return g;
+	}
 }
