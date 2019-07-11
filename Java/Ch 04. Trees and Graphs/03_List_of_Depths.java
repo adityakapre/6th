b0e9a9ddf660afb890dd63dd5df3e507cddde889
comeon.getit <adityakapre2009@gmail.com>
2019-07-11 11:16:30 -0400

    Update 03_List_of_Depths.java

--- Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
+++ Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
@@ -54,19 +54,6 @@ public class QuestionBFS {
 		}
 		return result;
 	}
-	
-	public static void printResult(ArrayList<LinkedList<TreeNode>> result){
-		int depth = 0;
-		for(LinkedList<TreeNode> entry : result) {
-			Iterator<TreeNode> i = entry.listIterator();
-			System.out.print("Link list at depth " + depth + ":");
-			while(i.hasNext()){
-				System.out.print(" " + ((TreeNode)i.next()).data);
-			}
-			System.out.println();
-			depth++;
-		}
-	}
 
 	public static void main(String[] args) {
 		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -104,19 +91,6 @@ public class QuestionDFS {
 		createLevelLinkedList(root, lists, 0);
 		return lists;
 	}	
-	
-	public static void printResult(ArrayList<LinkedList<TreeNode>> result){
-		int depth = 0;
-		for(LinkedList<TreeNode> entry : result) {
-			Iterator<TreeNode> i = entry.listIterator();
-			System.out.print("Link list at depth " + depth + ":");
-			while(i.hasNext()){
-				System.out.print(" " + ((TreeNode)i.next()).data);
-			}
-			System.out.println();
-			depth++;
-		}
-	}
 	
 	public static void main(String[] args) {
 		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
