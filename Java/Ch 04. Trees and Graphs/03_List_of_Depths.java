534a87dfae9ffd30d72ca37315c45d0bb81ec75e
comeon.getit <adityakapre2009@gmail.com>
2019-07-11 11:10:57 -0400

    Update and rename Java/Ch 04. Trees and Graphs/Q4_03_List_of_Depths/03_List_of_Depths.java to Java/Ch 04. Trees and Graphs/03_List_of_Depths.java

--- Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
+++ Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
@@ -4,62 +4,13 @@ List of Depths: Given a binary tree, design an algorithm which creates a linked 
 at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
 
 A:
-Use dfs
-*/
-public class QuestionDFS {
-
-	public static void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
-		if (root == null) return;
-		LinkedList<TreeNode> list = null;
-		if (lists.size() == level) { // Level not contained in list
-			list = new LinkedList<TreeNode>();
-			/* Levels are always traversed in order. So, if this is the first time we've visited level i,
-			 * we must have seen levels 0 through i - 1. We can therefore safely add the level at the end. */
-			lists.add(list);  
-		} else {
-			list = lists.get(level);
-		}
-		list.add(root);
-		createLevelLinkedList(root.left, lists, level + 1);
-		createLevelLinkedList(root.right, lists, level + 1);
-	}
-	
-	public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
-		ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
-		createLevelLinkedList(root, lists, 0);
-		return lists;
-	}	
-	
-	public static void printResult(ArrayList<LinkedList<TreeNode>> result){
-		int depth = 0;
-		for(LinkedList<TreeNode> entry : result) {
-			Iterator<TreeNode> i = entry.listIterator();
-			System.out.print("Link list at depth " + depth + ":");
-			while(i.hasNext()){
-				System.out.print(" " + ((TreeNode)i.next()).data);
-			}
-			System.out.println();
-			depth++;
-		}
-	}
-	
-	public static void main(String[] args) {
-		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
-		ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
-		printResult(list);
-	}
-}
-
-/*
-A:
 Alternatively, we can also implement a modification of breadth-first search. With this implementation, we
 want to iterate through the root first, then level 2, then level 3, and so on.
 With each level i, we will have already fully visited all nodes on level i-1. This means that to get which
 nodes are on level i, we can simply look at all children of the nodes of level i-1.
 
-first solution = dfs
-seconf solution = bfs
+first solution = bfs
+seconf solution = dfs
 
 One might ask which of these solutions is more efficient. Both run in O (N) time, but what about the space
 efficiency? At first, we might want to claim that the second solution is more space efficient.
@@ -127,3 +78,54 @@ public class QuestionBFS {
 	}
 
 }
+
+/*
+A:
+Use dfs
+*/
+public class QuestionDFS {
+
+	public static void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
+		if (root == null) return;
+		LinkedList<TreeNode> list = null;
+		if (lists.size() == level) { // Level not contained in list
+			list = new LinkedList<TreeNode>();
+			/* Levels are always traversed in order. So, if this is the first time we've visited level i,
+			 * we must have seen levels 0 through i - 1. We can therefore safely add the level at the end. */
+			lists.add(list);  
+		} else {
+			list = lists.get(level);
+		}
+		list.add(root);
+		createLevelLinkedList(root.left, lists, level + 1);
+		createLevelLinkedList(root.right, lists, level + 1);
+	}
+	
+	public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
+		createLevelLinkedList(root, lists, 0);
+		return lists;
+	}	
+	
+	public static void printResult(ArrayList<LinkedList<TreeNode>> result){
+		int depth = 0;
+		for(LinkedList<TreeNode> entry : result) {
+			Iterator<TreeNode> i = entry.listIterator();
+			System.out.print("Link list at depth " + depth + ":");
+			while(i.hasNext()){
+				System.out.print(" " + ((TreeNode)i.next()).data);
+			}
+			System.out.println();
+			depth++;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
+		ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
+		printResult(list);
+	}
+}
+
+
