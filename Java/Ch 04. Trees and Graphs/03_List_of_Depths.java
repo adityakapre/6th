5a48b5dd3218e3878f84fc45d49d8334f0dd062c
comeon.getit <adityakapre2009@gmail.com>
2019-07-11 11:13:41 -0400

    Update 03_List_of_Depths.java

--- Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
+++ Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
@@ -87,16 +87,16 @@ public class QuestionDFS {
 
 	public static void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
 		if (root == null) return;
-		LinkedList<TreeNode> list = null;
+		LinkedList<TreeNode> currLvl = null;
 		if (lists.size() == level) { // Level not contained in list
-			list = new LinkedList<TreeNode>();
+			currLvl = new LinkedList<TreeNode>();
 			/* Levels are always traversed in order. So, if this is the first time we've visited level i,
 			 * we must have seen levels 0 through i - 1. We can therefore safely add the level at the end. */
-			lists.add(list);  
+			lists.add(currLvl);  
 		} else {
-			list = lists.get(level);
+			currLvl = lists.get(level);
 		}
-		list.add(root);
+		currLvl.add(root);
 		createLevelLinkedList(root.left, lists, level + 1);
 		createLevelLinkedList(root.right, lists, level + 1);
 	}
