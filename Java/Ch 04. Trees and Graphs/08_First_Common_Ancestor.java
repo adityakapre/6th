5a86877116a4e4050cc84fb6f6ac1ebc3d90a01b
comeon.getit <adityakapre2009@gmail.com>
2019-07-11 23:51:31 -0400

    Update and rename Java/Ch 04. Trees and Graphs/Q4_08_First_Common_Ancestor/08_First_Common_Ancestor.java to Java/Ch 04. Trees and Graphs/08_First_Common_Ancestor.java

--- Java/Ch 04. Trees and Graphs/08_First_Common_Ancestor.java
+++ Java/Ch 04. Trees and Graphs/08_First_Common_Ancestor.java
@@ -125,5 +125,66 @@ public class QuestionC {
 		TreeNode ancestor = commonAncestor(root, n3, n7);
 		System.out.println(ancestor.data);
 	}
+}
+
+/*
+Solution #3: Without Links to Parents
+Alternatively, you could follow a chain in which p and q are on the same side. That is, if p and q are both on
+the left of the node, branch left to look for the common ancestor. If they are both on the right, branch right
+to look for the common ancestor. When p and q are no longer on the same side, you must have found the
+first common ancestor.
+This algorithm runs in O(n) time on a balanced tree. This is because covers is called on 2n nodes in the
+first call (n nodes for the left side, and n nodes for the right side). After that the algorithm branches left or
+right, at which point covers will be called on 2n/2 nodes, then 2n/4, and so on. This results in a runtime
+of O(n).
+We know at this point that we cannot do better than that in terms of the asymptotic runtime since we need
+to potentially look at every node in the tree. However, we may be able to improve it by a constant multiple.
+*/
+
+package Q4_08_First_Common_Ancestor;
+
+import CtCILibrary.TreeNode;
+
+public class QuestionD {
+	
+	public static TreeNode commonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (!hasNode(root, p) || !hasNode(root, q)) { // Error check - one node is not in tree
+			return null;
+		}
+		return ancestorHelper(root, p, q);
+	}
+	
+	//main logic
+	public static TreeNode ancestorHelper(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || root == p || root == q) {
+			return root;
+		}
+		boolean pIsOnLeft = hasNode(root.left, p);
+		boolean qIsOnLeft = hasNode(root.left, q);
+		if (pIsOnLeft != qIsOnLeft) { // Nodes are on different side
+			return root;
+		}
+		if(pIsOnLeft) {
+            		return lca(root.left, p, q);
+        	} else {
+            		return lca(root.right, p, q2);
+        	}
+	}
+	
+	//checks if n is within tree rooted at root
+	public static boolean hasNode(TreeNode root, TreeNode n) { 
+		if (root == null) return false;
+		if (root == n) return true;
+		return hasNode(root.left, n) || hasNode(root.right, n); 
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		TreeNode root = TreeNode.createMinimalBST(array);
+		TreeNode n3 = root.find(1);
+		TreeNode n7 = root.find(7);
+		TreeNode ancestor = commonAncestor(root, n3, n7);
+		System.out.println(ancestor.data);
+	}
 
 }
