d9d9f8c47d8ddc71689a2dcf1e7bd66070025996
comeon.getit <adityakapre2009@gmail.com>
2019-07-11 23:45:50 -0400

    Update and rename ans1.java to 08_First_Common_Ancestor.java

--- Java/Ch 04. Trees and Graphs/Q4_08_First_Common_Ancestor/08_First_Common_Ancestor.java
+++ Java/Ch 04. Trees and Graphs/Q4_08_First_Common_Ancestor/08_First_Common_Ancestor.java
@@ -1,4 +1,10 @@
 /*
+Q:
+First Common Ancestor: Design an algorithm and write code to find the first common ancestor
+of two nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not
+necessarily a binary search tree.
+
+A:
 If this were a binary search tree, we could modify the find operation for the two nodes and see where the
 paths diverge. Unfortunately, this is not a binary search tree, so we must try other approaches.
 Let's assume we're looking for the common ancestor of nodes p and q. One question to ask here is if each
