a7efd8b7dba987d1c94d54721aec0bdc4c80b65d
comeon.getit <adityakapre2009@gmail.com>
2019-07-17 17:28:14 -0400

    Update QuestionA.java

--- Java/Ch 04. Trees and Graphs/Q4_12_Paths_with_Sum/QuestionA.java
+++ Java/Ch 04. Trees and Graphs/Q4_12_Paths_with_Sum/QuestionA.java
@@ -1,4 +1,11 @@
 /*
+Q:
+Paths with Sum: You are given a binary tree in which each node contains an integer value (which
+might be positive or negative). Design an algorithm to count the number of paths that sum to a
+given value. The path does not need to start or end at the root or a leaf, but it must go downwards
+(traveling only from parent nodes to child nodes).
+
+A:
 One option is the brute force approach.
 Solution #1: Brute Force
 In the brute force approach, we just look at all possible paths. To do this, we traverse to each node. At each
