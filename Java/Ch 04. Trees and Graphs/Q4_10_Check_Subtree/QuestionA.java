7606b7a34eb747c216c9339a565d7760f2849357
comeon.getit <adityakapre2009@gmail.com>
2019-07-17 15:42:25 -0400

    Update QuestionA.java

--- Java/Ch 04. Trees and Graphs/Q4_10_Check_Subtree/QuestionA.java
+++ Java/Ch 04. Trees and Graphs/Q4_10_Check_Subtree/QuestionA.java
@@ -1,4 +1,11 @@
 /*
+Q:
+Check Subtree: Tl and T2 are two very large binary trees, with T1 much bigger than T2. Create an
+algorithm to determine if T2 is a subtree of T1.
+A tree T2 is a subtree of T1 if there exists a node n in T1 such that the subtree of n is identical to T2.
+That is, if you cut off the tree at node n, the two trees would be identical.
+
+A:
 In problems like this, it's useful to attempt to solve the problem assuming that there is just a small amount
 of data. This will give us a basic idea of an approach that might work.
 
@@ -59,13 +66,11 @@ import CtCILibrary.TreeNode;
 public class QuestionA {
 	
 	public static boolean containsTree(TreeNode t1, TreeNode t2) {
-		StringBuilder string1 = new StringBuilder();
-		StringBuilder string2 = new StringBuilder();
-		
-		getOrderString(t1, string1);
-		getOrderString(t2, string2);
-		
-		return string1.indexOf(string2.toString()) != -1;
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder();
+		getOrderString(t1, sb1);
+		getOrderString(t2, sb2);
+		return sb1.indexOf(sb2.toString()) != -1;
 	}
 	
 	public static void getOrderString(TreeNode node, StringBuilder sb) {
