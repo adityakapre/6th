b75ba8f3b61855b7fc03cb53d9aff4093fda0e88
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 17:57:59 -0400

    Update MyObject.java

--- Java/Ch 15. Threads and Locks/IntroductionWaitNotify/MyObject.java
+++ Java/Ch 15. Threads and Locks/IntroductionWaitNotify/MyObject.java
@@ -1,13 +1,24 @@
-package IntroductionWaitNotify;
+package synchronization;
 
 public class MyObject {
+	
+	private static int callerNumber=1;
+	
 	public void foo(String name) {
-		try {
-			System.out.println("Thread " + name + ".foo(): starting");
-			Thread.sleep(3000);
-			System.out.println("Thread " + name + ".foo(): ending");
-		} catch (InterruptedException exc) {
-			System.out.println("Thread " + name + ": interrupted.");
+		synchronized(this) {
+			try {
+				callerNumber++;
+				System.out.println("Thread " + name + ".foo(): starting logic execution");
+				Thread.sleep(3000);
+				System.out.println("Thread " + name + ".foo(): ending");
+				Thread.sleep(3000);
+			} catch (InterruptedException exc) {
+				System.out.println("Thread " + name + ": interrupted.");
+			}
 		}
 	} 	
+	
+	public static synchronized int getCallerNumber() {
+		return callerNumber;
+	}
 }
