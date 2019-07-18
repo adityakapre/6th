30be05c707349a91537270f08876299d6ed670a6
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 15:48:16 -0400

    Update FBThread.java

--- Java/Ch 15. Threads and Locks/Q15_07_FizzBuzz/FBThread.java
+++ Java/Ch 15. Threads and Locks/Q15_07_FizzBuzz/FBThread.java
@@ -1,11 +1,99 @@
 /*
+Q:
 FizzBuzz: In the classic problem FizzBuzz, you are told to print the numbers from 1 to n. However,
 when the number is divisible by 3, print "Fizz''. When it is divisible by 5, print "Buzz''. When it is
 divisible by 3 and 5, print"FizzBuzz''. In this problem, you are asked to do this in a multithreaded way.
 Implement a multithreaded version of FizzBuzz with four threads. One thread checks for divisibility
 of 3 and prints"Fizz''. Another thread is responsible for divisibility of 5 and prints"Buzz''. A third thread
 is responsible for divisibility of 3 and 5 and prints "FizzBuzz''. A fourth thread does the numbers.
+
+A:
+Although this problem (in the single threaded version) shouldn't be hard, a lot of candidates overcomplicate
+it. They look for something "beautiful"that reuses the fact that the divisible by 3 and 5 case ("FizzBuzz")
+seems to resemble the individual cases ("Fizz" and "Buzz").
 */
+public class QuestionA {
+
+	public static void fizzbuzz(int n) {
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		fizzbuzz(100);
+	}
+
+}
+
+/*
+A:
+Multithreaded
+To do this multithreaded, we want a structure that looks something like this:
+
+FizzBuzz Thread
+if i div by 3 && 5
+print FizzBuzz
+increment i
+repeat until i > n
+
+Fizz Thread
+if i div by only 3
+print Fizz
+increment i
+repeat until i > n
+
+Buzz Thread
+if i div by only 5
+print Buzz
+increment i
+repeat until i > n
+
+Nunber Thread
+if i not div by 3 or 5
+print i
+increment i
+repeat until i > n
+
+The code for this will look something like:
+1 while (true) {
+2 if (current> max) {
+3 return;
+4 }
+5 if(/divisibility test/) {
+6 System.out.println(/print something/);
+7 current++;
+8 }
+9 }
+
+We'll need to add some synchronization in the loop. Otherwise, the value of current could change
+between lines 2 - 4 and lines 5 - 8, and we can inadvertently exceed the intended bounds of the loop. 
+Additionally, incrementing is not thread-safe.
+To actually implement this concept, there are many possibilities. One possibility is to have four entirely
+separate thread classes that share a reference to the current variable (which can be wrapped in an object).
+The loop for each thread is substantially similar. They just have different target values for the divisibility
+checks, and different print values.
+
+                 FizzBuzz  Fizz   Buzz   Number
+current%3==0     true 	   true   false  false
+current%5==0     true      false  true   false
+to print         FizzBuzz  Fizz   Buzz   current
+
+For the most part, this can be handled by taking in "target" parameters and the value to print. The output for
+the Number thread needs to be overwritten, though, as it's not a simple, fixed string.
+We can implement a FizzBuzzThread class which handles most of this. A Number Thread class can
+extend FizzBuzzThread and override the print method.
+
+*/
+
 package Q15_07_FizzBuzz;
 
 import java.util.function.Function;
