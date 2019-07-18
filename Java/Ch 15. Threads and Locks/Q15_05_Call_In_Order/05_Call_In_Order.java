4856a0134dc643ceb4e81a59f7f9ca9f8086f3b8
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 16:06:47 -0400

    Update 05_Call_In_Order.java

--- Java/Ch 15. Threads and Locks/Q15_05_Call_In_Order/05_Call_In_Order.java
+++ Java/Ch 15. Threads and Locks/Q15_05_Call_In_Order/05_Call_In_Order.java
@@ -11,9 +11,69 @@ The same instance of Foo will be passed to three different threads. ThreadA will
 will call second, and thread( will call third. Design a mechanism to ensure that first is called
 before second and second is called before third.
 
+A:
+The general logic is to check if first() has completed before executing second(), and if second()
+has completed before calling third (). Because we need to be very careful about thread safety, simple
+boolean flags won't do the job.
 
+What about using a lock to do something like the below code?
 */
-package Q15_05_Call_In_Order;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FooBad {
+	public int pauseTime = 1000;
+	public ReentrantLock lock1;
+	public ReentrantLock lock2;
+	
+	public FooBad() {	
+		try {
+			lock1 = new ReentrantLock();
+			lock2 = new ReentrantLock();
+			
+			lock1.lock();
+			lock2.lock();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void first() {
+		try {
+			System.out.println("Started Executing 1");
+			Thread.sleep(pauseTime);
+			System.out.println("Finished Executing 1");
+			lock1.unlock();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void second() {
+		try {
+			lock1.lock();
+			lock1.unlock();
+			System.out.println("Started Executing 2");
+			Thread.sleep(pauseTime);
+			System.out.println("Finished Executing 2");
+			lock2.unlock();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}	
+	
+	public void third() {
+		try {
+			lock2.lock();
+			lock2.unlock();
+			System.out.println("Started Executing 3");
+			Thread.sleep(pauseTime);
+			System.out.println("Finished Executing 3");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}	
+}
 
 public class Question {
 	public static void main(String[] args) {
@@ -28,3 +88,11 @@ public class Question {
 		thread1.start();
 	}
 }
+/*
+This code won't actually quite work due to the concept of lock ownership. One thread is actually performing
+the lock (in the Fo oBad constructor), but different threads attempt to unlock the locks. This is not allowed,
+and your code will raise an exception. A lock in Java is owned by the same thread which locked it.
+
+A2:
+Instead, we can replicate this behavior with semaphores. The logic is identical as below.
+*/
