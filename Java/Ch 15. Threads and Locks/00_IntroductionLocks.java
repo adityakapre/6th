79f1eb369caa3a6ff4511baf578a976a8055c809
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 21:00:41 -0400

    Update 00_IntroductionLocks.java

--- Java/Ch 15. Threads and Locks/00_IntroductionLocks.java
+++ Java/Ch 15. Threads and Locks/00_IntroductionLocks.java
@@ -46,7 +46,8 @@ class LockedATM {
 			temp = temp - value;
 			Thread.sleep(100);
 			balance = temp;
-		} catch (InterruptedException e) {		}
+		} catch (InterruptedException e) {		
+		}
 		lock.unlock();
 		return temp;
 	}
@@ -59,7 +60,8 @@ class LockedATM {
 			temp = temp + value;
 			Thread.sleep(100);
 			balance = temp;
-		} catch (InterruptedException e) {		}
+		} catch (InterruptedException e) {		
+		}
 		lock.unlock();
 		return temp;
 	} 
