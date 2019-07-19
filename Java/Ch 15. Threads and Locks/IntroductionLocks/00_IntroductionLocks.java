ad628367bec4df275632def30a21fce401e48abe
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 20:35:35 -0400

    Update 00_IntroductionLocks.java

--- Java/Ch 15. Threads and Locks/IntroductionLocks/00_IntroductionLocks.java
+++ Java/Ch 15. Threads and Locks/IntroductionLocks/00_IntroductionLocks.java
@@ -74,44 +74,35 @@ public class LockedATM {
 
 package IntroductionLocks;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
-public class LockedATM {
-	private Lock lock;
+public class NoLockATM {
 	private int balance = 100;
 	
-	public LockedATM() {
-		lock = new ReentrantLock();
+	public NoLockATM() {
 	}
 	
 	public int withdraw(int value) {
-		lock.lock();
 		int temp = balance;
 		try {
-			Thread.sleep(100);
+			Thread.sleep(300);
 			temp = temp - value;
-			Thread.sleep(100);
+			Thread.sleep(300);
 			balance = temp;
 		} catch (InterruptedException e) {		}
-		lock.unlock();
 		return temp;
 	}
 	
 	public int deposit(int value) {
-		lock.lock();
 		int temp = balance;
 		try {
-			Thread.sleep(100);
+			Thread.sleep(300);
 			temp = temp + value;
-			Thread.sleep(100);
+			Thread.sleep(300);
 			balance = temp;
 		} catch (InterruptedException e) {		}
-		lock.unlock();
 		return temp;
 	} 
 	
 	public int getBalance() {
 		return balance;
-	}
+	}	
 }
