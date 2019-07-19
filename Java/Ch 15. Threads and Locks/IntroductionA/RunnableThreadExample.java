25a131fed9241122b1259db01ac8d9f25559bd30
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 21:08:10 -0400

    Update RunnableThreadExample.java

--- Java/Ch 15. Threads and Locks/IntroductionA/RunnableThreadExample.java
+++ Java/Ch 15. Threads and Locks/IntroductionA/RunnableThreadExample.java
@@ -17,3 +17,24 @@ public class RunnableThreadExample implements Runnable {
 		System.out.println("RunnableThread terminating.");
 	}
 }
+
+public class ExampleA {
+
+	public static void main(String[] args) {
+		RunnableThreadExample instance = new RunnableThreadExample();
+		Thread thread = new Thread(instance);
+		thread.start();
+		
+		/* waits until earlier thread counts to 5 (slowly) */
+		while (instance.count != 5) {		
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException exc) {
+				exc.printStackTrace();
+			}
+		}
+		
+		System.out.println("Program Terminating.");
+	}
+}
+
