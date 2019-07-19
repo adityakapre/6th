1c84f69e83c9ffa8af578a6dc1f1e7f6266c45e6
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 20:33:46 -0400

    Update and rename Intro.java to  00_IntroductionLocks.java

---
+++ Java/Ch 15. Threads and Locks/IntroductionLocks/ 00_IntroductionLocks.java
@@ -0,0 +1,73 @@
+package IntroductionLocks;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		NoLockATM noLockATM = new NoLockATM();
+		LockedATM lockedATM = new LockedATM();
+		MyClass thread1 = new MyClass(noLockATM, lockedATM);
+		MyClass thread2 = new MyClass(noLockATM, lockedATM);
+		
+		thread1.start();
+		thread2.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		thread1.waitUntilDone();
+		thread2.waitUntilDone();
+		
+		System.out.println("NoLock ATM: " + noLockATM.getBalance());
+		System.out.println("Locked ATM: " + lockedATM.getBalance());
+		int v = thread1.delta + thread2.delta + 100;
+		System.out.println("Should Be: " + v);
+		System.out.println("Program terminating.");
+	}
+}
+
+package IntroductionLocks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockedATM {
+	private Lock lock;
+	private int balance = 100;
+	
+	public LockedATM() {
+		lock = new ReentrantLock();
+	}
+	
+	public int withdraw(int value) {
+		lock.lock();
+		int temp = balance;
+		try {
+			Thread.sleep(100);
+			temp = temp - value;
+			Thread.sleep(100);
+			balance = temp;
+		} catch (InterruptedException e) {		}
+		lock.unlock();
+		return temp;
+	}
+	
+	public int deposit(int value) {
+		lock.lock();
+		int temp = balance;
+		try {
+			Thread.sleep(100);
+			temp = temp + value;
+			Thread.sleep(100);
+			balance = temp;
+		} catch (InterruptedException e) {		}
+		lock.unlock();
+		return temp;
+	} 
+	
+	public int getBalance() {
+		return balance;
+	}
+}
