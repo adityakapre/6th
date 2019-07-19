12886b6730f34e68f6b603e09aa93c9309313446
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 21:38:22 -0400

    Update Intro.java

--- Java/Ch 15. Threads and Locks/IntroductionSynchronizedBlocks/Intro.java
+++ Java/Ch 15. Threads and Locks/IntroductionSynchronizedBlocks/Intro.java
@@ -34,3 +34,17 @@ public class MyClass extends Thread  {
 		myObj.foo(name);
 	}
 }
+
+public class MyObject {
+	public void foo(String name) {
+		synchronized(this) {
+			try {
+				System.out.println("Thread " + name + ".foo(): starting");
+				Thread.sleep(3000);
+				System.out.println("Thread " + name + ".foo(): ending");
+			} catch (InterruptedException exc) {
+				System.out.println("Thread " + name + ": interrupted.");
+			}
+		}
+	} 	
+}
