b634ffe8662bf6a897011c9d4b061c2dd94309ea
comeon.getit <adityakapre2009@gmail.com>
2019-07-18 21:10:06 -0400

    Update and rename Java/Ch 15. Threads and Locks/IntroductionB/ThreadExample.java to Java/Ch 15. Threads and Locks/00_ThreadExample.java

--- Java/Ch 15. Threads and Locks/00_ThreadExample.java
+++ Java/Ch 15. Threads and Locks/00_ThreadExample.java
@@ -18,3 +18,17 @@ public class ThreadExample extends Thread {
 	}
 }
 
+public class ExampleB {
+	public static void main(String args[]) {
+		ThreadExample instance = new ThreadExample();
+		instance.start();
+		
+		while (instance.count != 5) {		
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException exc) {
+				exc.printStackTrace();
+			}
+		}
+	}
+}
