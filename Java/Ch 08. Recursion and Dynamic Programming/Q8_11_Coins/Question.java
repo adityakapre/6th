cedead7f3c3634ae4952c2e8b81e7a6ce28764be
comeon.getit <adityakapre2009@gmail.com>
2019-07-22 17:04:04 -0400

    Update Question.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_11_Coins/Question.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_11_Coins/Question.java
@@ -257,3 +257,78 @@ public class QuestionB {
 	}
 
 }
+
+/*
+1) Optimal Substructure
+To count the total number of solutions, we can divide all set solutions into two sets.
+1) Solutions that do not contain mth coin (or Sm).
+2) Solutions that contain at least one Sm.
+Let count(S[], m, n) be the function to count the number of solutions, then it can be written as sum of count(S[], m-1, n) 
+and count(S[], m, n-Sm).
+
+Therefore, the problem has optimal substructure property as the problem can be solved using solutions to subproblems.
+
+2) Overlapping Subproblems
+Following is a simple recursive implementation of the Coin Change problem. The implementation simply follows 
+the recursive structure mentioned above.
+*/
+
+// Recursive java program for coin change problem. 
+import java.io.*; 
+  
+class GFG { 
+      
+    // Returns the count of ways we can sum S[0...m-1] coins to get sum n 
+    static int count( int S[], int m, int n ) { 
+        // If n is 0 then there is 1 solution (do not include any coin) 
+        if (n == 0) 
+            return 1; 
+          
+        // If n is less than 0 then no solution exists 
+        if (n < 0) 
+            return 0; 
+      
+        // If there are no coins and n  
+        // is greater than 0, then no 
+        // solution exist 
+        if (m <=0 && n >= 1) 
+            return 0; 
+      
+        // count is sum of solutions   
+        // (i) including S[m-1] (ii) excluding S[m-1] 
+        return count( S, m - 1, n ) + count( S, m, n-S[m-1] ); 
+    } 
+      
+    // Driver program to test above function 
+    public static void main(String[] args) { 
+        int arr[] = {1, 2, 3}; 
+        int m = arr.length; 
+        System.out.println( count(arr, m, 4)); 
+    } 
+} 
+
+/*
+Since same suproblems are called again, this problem has Overlapping Subprolems property. 
+So the Coin Change problem has both properties (see Overlapping Subproblems and Optimal Substructure) of a dynamic programming problem. 
+Like other typical Dynamic Programming(DP) problems, recomputations of same subproblems can be avoided 
+by constructing a temporary array table[][] in bottom up manner.
+*/
+
+public static int count( int S[], int m, int n ) { 
+    // table[i] will be storing the number of solutions for 
+    // value i. We need n+1 rows as the table is constructed 
+    // in bottom up manner using the base case (n = 0) 
+    int table[]=new int[n+1]; 
+  
+    // Base case (If given value is 0) 
+    table[0] = 1; 
+  
+    // Pick all coins one by one and update the table[] values 
+    // after the index greater than or equal to the value of the 
+    // picked coin 
+    for(int i=0; i<m; i++) 
+        for(int j=S[i]; j<=n; j++) 
+            table[j] += table[j-S[i]]; 
+  
+    return table[n]; 
+} 
