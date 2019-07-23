ec5e2f59deba7c1512885caa8cd053837fd596ef
comeon.getit <adityakapre2009@gmail.com>
2019-07-22 23:15:59 -0400

    Update QuestionA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_13_Stack_of_Boxes/QuestionA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_13_Stack_of_Boxes/QuestionA.java
@@ -171,5 +171,62 @@ public class QuestionB {
 		int height = createStack(boxes);
 		System.out.println(height);
 	}
+}
+/*
+Solution #2
+Alternatively, we can think about the recursive algorithm as making a choice, at each step, whether to put
+a particular box in the stack. (We will again sort our boxes in descending order by a dimension, such as
+height.)
+First, we choose whether or not to put box O in the stack. Take one recursive path with box O at the bottom
+and one recursive path without box 0. Return the better of the two options.
+Then, we choose whether or not to put box 1 in the stack. Take one recursive path with box 1 at the bottom
+and one path without box 1. Return the better of the two options. 
+We will again use memoization to cache the height of the tallest stack with a particular bottom.
+*/
+package Q8_13_Stack_of_Boxes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class QuestionC {	
+	public static int createStack(ArrayList<Box> boxes) {
+		Collections.sort(boxes, new BoxComparator());
+		int[] stackMap = new int[boxes.size()];
+		return createStack(boxes, null, 0, stackMap);
+	}
+	
+	public static int createStack(ArrayList<Box> boxes, Box bottom, int offset, int[] stackMap) {
+		if (offset >= boxes.size()) {
+			return 0;
+		}
+		
+		/* height with this bottom */
+		Box newBottom = boxes.get(offset);
+		int heightWithBottom = 0;
+		if (bottom == null || newBottom.canBeAbove(bottom)) {
+			if (stackMap[offset] == 0) {
+				stackMap[offset] = createStack(boxes, newBottom, offset + 1, stackMap);
+				stackMap[offset] += newBottom.height;
+			}
+			heightWithBottom = stackMap[offset];
+		}
+		
+		/* without this bottom */
+		int heightWithoutBottom = createStack(boxes, bottom, offset + 1, stackMap);
+		
+		return Math.max(heightWithBottom, heightWithoutBottom);
+	}
+		
+	
+	public static void main(String[] args) {
+		Box[] boxList = { new Box(6, 4, 4), new Box(8, 6, 2), new Box(5, 3, 3), new Box(7, 8, 3), new Box(4, 2, 2), new Box(9, 7, 3)};
+		ArrayList<Box> boxes = new ArrayList<Box>();
+		for (Box b : boxList) {
+			boxes.add(b);
+		}
+		
+		int height = createStack(boxes);
+		System.out.println(height);
+	}
 
 }
