b5cf7474003d982f8f7b7a1b3977cba05451b701
comeon.getit <adityakapre2009@gmail.com>
2019-07-31 12:11:49 -0400

    Update 03_List_of_Depths.java

--- Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
+++ Java/Ch 04. Trees and Graphs/03_List_of_Depths.java
@@ -4,13 +4,54 @@ List of Depths: Given a binary tree, design an algorithm which creates a linked 
 at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
 
 A:
+Though we might think at first glance that this problem requires a level-by-level traversal, this isn't actually
+necessary. We can traverse the graph any way that we'd like, provided we know which level we're on as we
+do so.
+We can implement a simple modification of the pre-order traversal algorithm, where we pass in 
+level + 1 to the next recursive call. The code below provides an implementation using depth-first search.
+*/
+public class QuestionDFS {
+
+	public static void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
+		if (root == null) return;
+		LinkedList<TreeNode> currLvl = null;
+		if (lists.size() == level) { // current level not contained in list
+			currLvl = new LinkedList<TreeNode>();
+			/* Levels are always traversed in order. So, if this is the first time we've visited level i,
+			 * we must have seen levels 0 through i - 1. We can therefore safely add the level at the end. */
+			lists.add(currLvl);  
+		} else {
+			currLvl = lists.get(level);
+		}
+		currLvl.add(root);
+		createLevelLinkedList(root.left, lists, level + 1);
+		createLevelLinkedList(root.right, lists, level + 1);
+	}
+	
+	public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
+		createLevelLinkedList(root, lists, 0);
+		return lists;
+	}	
+	
+	public static void main(String[] args) {
+		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
+		ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
+		printResult(list);
+	}
+}
+
+
+/*
+A:
 Alternatively, we can also implement a modification of breadth-first search. With this implementation, we
 want to iterate through the root first, then level 2, then level 3, and so on.
 With each level i, we will have already fully visited all nodes on level i-1. This means that to get which
 nodes are on level i, we can simply look at all children of the nodes of level i-1.
 
-first solution = bfs
-seconf solution = dfs
+first solution = dfs
+seconf solution = bfs
 
 One might ask which of these solutions is more efficient. Both run in O (N) time, but what about the space
 efficiency? At first, we might want to claim that the second solution is more space efficient.
@@ -19,7 +60,6 @@ adds a new level to the stack. The second solution, which is iterative, does not
 However, both solutions require returning O(N) data. The extra 0(log N) space usage from the recursive
 implementation is dwarfed by the O (N) data that must be returned. So while the first solution may actually
 use more data, they are equally efficient when it comes to "big O:'
-
 */
 package Q4_03_List_of_Depths;
 
@@ -55,43 +95,6 @@ public class QuestionBFS {
 		return result;
 	}
 
-	public static void main(String[] args) {
-		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
-		ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
-		printResult(list);
-	}
-
-}
-
-/*
-A:
-Use dfs
-*/
-public class QuestionDFS {
-
-	public static void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
-		if (root == null) return;
-		LinkedList<TreeNode> currLvl = null;
-		if (lists.size() == level) { // current level not contained in list
-			currLvl = new LinkedList<TreeNode>();
-			/* Levels are always traversed in order. So, if this is the first time we've visited level i,
-			 * we must have seen levels 0 through i - 1. We can therefore safely add the level at the end. */
-			lists.add(currLvl);  
-		} else {
-			currLvl = lists.get(level);
-		}
-		currLvl.add(root);
-		createLevelLinkedList(root.left, lists, level + 1);
-		createLevelLinkedList(root.right, lists, level + 1);
-	}
-	
-	public static ArrayList<LinkedList<TreeNode>> createLevelLinkedList(TreeNode root) {
-		ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
-		createLevelLinkedList(root, lists, 0);
-		return lists;
-	}	
-	
 	public static void main(String[] args) {
 		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
