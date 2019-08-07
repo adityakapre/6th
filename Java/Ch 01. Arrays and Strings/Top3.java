17666fc330e25ab3aa31c09867718cbd886fb9ac
comeon.getit <adityakapre2009@gmail.com>
2019-08-07 15:48:23 -0400

    Update Top3.java

--- Java/Ch 01. Arrays and Strings/Top3.java
+++ Java/Ch 01. Arrays and Strings/Top3.java
@@ -7,18 +7,17 @@ public void findTop3 {
     int h1 = INT_MIN; //TOP 1st
     int h2 = INT_MIN; //TOP 2nd
     int h3 = INT_MIN; //TOP 3rd
-    for(int i=0;i<10;i++)
-    {
-        if(a[i] > h1)
-        {
-            h3 = h2; h2 = h1; h1 = a[i];
+    for(int i=0;i<10;i++) {
+        if(a[i] > h1) {
+            h3 = h2; 
+            h2 = h1; 
+            h1 = a[i];
         }
-        else if (a[i] > h2)
-        {
-            h3 = h2; h2 = a[i];
+        else if (a[i] > h2) {
+            h3 = h2; 
+            h2 = a[i];
         }
-        else if  (a[i] > h3)
-        {
+        else if  (a[i] > h3) {
             h3 = a[i];
         }
     }
