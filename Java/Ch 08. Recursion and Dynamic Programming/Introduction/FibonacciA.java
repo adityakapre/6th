997e4a1911a65dbfb9987cf9663800501da88cc1
comeon.getit <adityakapre2009@gmail.com>
2019-08-14 10:43:23 -0400

    Update FibonacciA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Introduction/FibonacciA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Introduction/FibonacciA.java
@@ -108,15 +108,17 @@ public class FibonacciB {
 }
 
 /*
-Iterative fibonacci
+Iterative fibonacci with memo table
 */
 package Introduction;
 
 public class FibonacciC {
 	
 	public static int fibonacci(int n) {
-		if (n == 0) return 0;
-		else if (n == 1) return 1;
+		if (n == 0) 
+			return 0;
+		else if (n == 1) 
+			return 1;
 		
 		int[] memo = new int[n];
 		memo[0] = 0;
@@ -127,6 +129,45 @@ public class FibonacciC {
 		return memo[n - 1] + memo[n - 2];
 	}
 
+	public static void main(String[] args) {
+		int max = 100; // Make this as big as you want! (Though you'll exceed the bounds of a long around 46)
+		int trials = 10; // Run code multiple times to compute average time.
+		double[] times = new double[max]; // Store times
+		
+		for (int j = 0; j < trials; j++) { // Run this 10 times to compute
+			for (int i = 0; i < max; i++) {
+				long start = System.currentTimeMillis();
+				System.out.println(fibonacci(i));
+				long end = System.currentTimeMillis();
+				long time = end - start;
+				times[i] += time; 
+			}
+		}
+		
+		for (int j = 0; j < max; j++) {
+			//System.out.println(j + ": " + times[j] / trials + "ms");
+		}
+	}
+}
+
+/*
+Iterative fibonacci without memo table
+*/
+package Introduction;
+
+public class FibonacciD {
+	public static int fibonacci(int n) {
+		if (n == 0) return 0;
+		int a = 0;
+		int b = 1;
+		for (int i = 2; i < n; i++) {
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+		return a + b;
+	}
+	
 	public static void main(String[] args) {
 		int max = 100; // Make this as big as you want! (Though you'll exceed the bounds of a long around 46)
 		int trials = 10; // Run code multiple times to compute average time.
