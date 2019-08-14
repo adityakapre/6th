73a1a8de8d1557c0024fe965c65f0bb5f53638c1
comeon.getit <adityakapre2009@gmail.com>
2019-08-14 10:40:00 -0400

    Update FibonacciA.java

--- Java/Ch 08. Recursion and Dynamic Programming/Introduction/FibonacciA.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Introduction/FibonacciA.java
@@ -1,11 +1,9 @@
 /*
-
 The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
 The next number is found by adding up the two numbers before it.
 f(n) = f(n-1) + f(n-2)
 
 for fibonacci w/o memoization
-
 suppose n=5, fibonacci(5)
 
 			         f(5)                       height=1
@@ -30,27 +28,21 @@ if t is empty,
 	height(t) = −1
 Else
 	height(t) = 1+ max (height(leftTree(t)), height(rightTree(t)))
-
-	      
-
 */
 
 package Introduction;
 
-public class FibonacciA {
-	public static int fibonacci(int i) {
-		if (i == 0) {
-			return 0;
-		}
-		if (i == 1) {
-			return 1;
-		}
-		return fibonacci(i - 1) + fibonacci(i - 2);
+	public class FibonacciA {
+		public static int fibonacci(int i) {
+			if (i == 0) {
+				return 0;
+			}
+			if (i == 1) {
+				return 1;
+			}
+			return fibonacci(i - 1) + fibonacci(i - 2);
 	}
 	
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		int max = 35; // WARNING: If you make this above 40ish, your computer may serious slow down.
 		int trials = 10; // Run code multiple times to compute average time.
@@ -71,5 +63,46 @@ public class FibonacciA {
 			System.out.println(j + ": " + times[j] / trials + "ms");
 		}
 	}
+}
 
+/*
+Fibonacci with memoization
+*/
+package Introduction;
+
+public class FibonacciB {
+
+	public static int fibonacci(int n) {
+		return fibonacci(n, new int[n + 1]);
+	}
+	
+	public static int fibonacci(int i, int[] memo) {
+		if (i == 0) return 0;
+		else if (i == 1) return 1;
+		
+		if (memo[i] == 0) {
+			memo[i] = fibonacci(i - 1, memo) + fibonacci(i - 2, memo);
+		}
+		return memo[i];
+	}
+	
+	public static void main(String[] args) {
+		int max = 100; // Make this as big as you want! (Though you'll exceed the bounds of a long around 46)
+		int trials = 10; // Run code multiple times to compute average time.
+		double[] times = new double[max]; // Store times
+		
+		for (int j = 0; j < trials; j++) { // Run this 10 times to compute
+			for (int i = 0; i < max; i++) {
+				long start = System.currentTimeMillis();
+				System.out.println(fibonacci(i));
+				long end = System.currentTimeMillis();
+				long time = end - start;
+				times[i] += time; 
+			}
+		}
+		
+		for (int j = 0; j < max; j++) {
+			//System.out.println(j + ": " + times[j] / trials + "ms");
+		}
+	}
 }
