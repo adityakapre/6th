9389f2f579677278a1e4d6daecad4f42bd19edce
comeon.getit <adityakapre2009@gmail.com>
2019-08-16 11:24:58 -0400

    Update 05_Sum_Lists.java

--- Java/Ch 02. Linked Lists/05_Sum_Lists.java
+++ Java/Ch 02. Linked Lists/05_Sum_Lists.java
@@ -53,8 +53,6 @@ public class QuestionA {
 		if (l1 == null && l2 == null && carry == 0) {
              		return null;
 		}
-		
-		LinkedListNode result = new LinkedListNode();
 		int value = carry;
 		if (l1 != null) {
 			value += l1.data;
@@ -62,6 +60,7 @@ public class QuestionA {
 		if (l2 != null) {
 			value += l2.data;
 		}
+		LinkedListNode result = new LinkedListNode();
 		result.data = value % 10;                         //modulus, returns remainder
 		if (l1 != null || l2 != null) {
 			LinkedListNode more = addLists(l1 == null ? null : l1.next, 
