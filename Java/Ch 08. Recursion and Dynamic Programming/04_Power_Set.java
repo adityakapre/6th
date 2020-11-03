b7bd4575bb90bc0998633ebcc5804611863c9e5e
Aditya Kapre <aditya14641@gmail.com>
2020-11-03 09:38:03 -0500

    Update 04_Power_Set.java

--- Java/Ch 08. Recursion and Dynamic Programming/04_Power_Set.java
+++ Java/Ch 08. Recursion and Dynamic Programming/04_Power_Set.java
@@ -2,6 +2,8 @@
 Q:
 Power Set: Write a method to return all subsets of a set.
 
+Ans: https://www.youtube.com/watch?v=LdtQAYdYLcE
+
 We should first have some reasonable expectations of our time and space complexity.
 How many subsets of a set are there? When we generate a subset, each element has the "choice" of either
 being in there or not. That is, for the first element, there are two choices: it is either in the set or it is not. For
