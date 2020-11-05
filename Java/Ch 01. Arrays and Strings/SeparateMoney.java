7ca549a768dcdf2be3ca882885613d8a9f39d151
Aditya Kapre <aditya14641@gmail.com>
2020-11-05 18:02:08 -0500

    Create SeparateMoney.java

---
+++ Java/Ch 01. Arrays and Strings/SeparateMoney.java
@@ -0,0 +1,5 @@
+/*
+Find a way to separate money into n parts. For example, if total money is 121, 
+find a way to separate 121 into 4 closest parts and the solution would be an 
+array with elements 30, 30, 30, 31
+*/
