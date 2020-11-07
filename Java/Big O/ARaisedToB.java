cb91e42530c4e0cb2b7e5560989195cb8bd7a412
Aditya Kapre <aditya14641@gmail.com>
2020-11-07 09:42:03 -0500

    Update ARaisedToB.java

--- Java/Big O/ARaisedToB.java
+++ Java/Big O/ARaisedToB.java
@@ -16,11 +16,11 @@ What is the time complexity ?
 0(b). The recursive code iterates through b calls, since it subtracts one at each level.
 */
 public static void main(String[] args) {
-		int a = 2;
-		int power = 3;
-		int ans = a;
-		for(int i=1; i<power; i++) {
-			ans=ans*a;
-		}
-		System.out.println("answer = "+ans);
+	int a = 2;
+	int power = 3;
+	int ans = a;
+	for(int i=1; i<power; i++) {
+		ans=ans*a;
+	}
+	System.out.println("answer = "+ans);
 }
