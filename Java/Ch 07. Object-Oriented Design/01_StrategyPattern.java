2322ab0f4c22268ed15521924c89f0f7dfc99f13
Aditya Kapre <aditya14641@gmail.com>
2020-11-12 18:33:29 -0500

    Create 01_StrategyPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/01_StrategyPattern.java
@@ -0,0 +1,6 @@
+https://www.youtube.com/watch?v=v9ejT8FO-7I&list=PLMygBPoOe_IV_42YFkuAECr3g7HhBXBTN&index=1
+
+- Encapsulates interchangeable behaviors and uses delegation to decide which one to use.
+  The strategy pattern defines a family of algorithms, encapsulates each one, and makes 
+  them interchangeable. Strategy lets the algorithm vary independently from the clients that use it.
+- Favour composition over inheritance
