34b2d5e7e0cccfc25d2a4f719c990954979a3107
Aditya Kapre <aditya14641@gmail.com>
2020-11-12 18:38:01 -0500

    Create 02_ObserverPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/02_ObserverPattern.java
@@ -0,0 +1,6 @@
+https://www.youtube.com/watch?v=_BpmfnqjgzQ&list=PLMygBPoOe_IV_42YFkuAECr3g7HhBXBTN&index=2
+
+- The subject and observer define one to many relationship.
+  The observers are dependent on subject such that when the subject state changes , 
+  the observers get notified.
+- Push vs poll
