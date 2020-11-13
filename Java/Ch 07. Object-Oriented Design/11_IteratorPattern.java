62ffcce9935c89c90d1d0510aaa18b6e42f03e5b
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 11:00:02 -0500

    Create 11_IteratorPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/11_IteratorPattern.java
@@ -0,0 +1,9 @@
+https://www.youtube.com/watch?v=uNTNEfwYXhI&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=16
+
+- Def:
+  Provides way to access elems of aggregate object sequentially without exposing 
+  its underlying representation.
+  
+- Class diagram:
+  https://youtu.be/uNTNEfwYXhI?list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&t=2083
+  
