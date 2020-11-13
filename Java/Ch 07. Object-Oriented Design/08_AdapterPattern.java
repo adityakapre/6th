417446141eb9c6d1ffae4dcaebd2ab50a906ec20
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 10:36:04 -0500

    Create 08_AdapterPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/08_AdapterPattern.java
@@ -0,0 +1,8 @@
+https://www.youtube.com/watch?v=2PKQtcJjYvc&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=8
+
+- Def:
+  Converts interface of a class into another interface client supports. Adapter lets classes 
+  work together that  couldn't otherwise because of incompatible interfaces.
+- Class diagram:
+  https://youtu.be/2PKQtcJjYvc?list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&t=472
+  
