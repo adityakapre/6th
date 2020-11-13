92eb523006fa6d2f4b61df8540f218e1c0f7f492
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 11:13:32 -0500

    Create 14_ProxyPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/14_ProxyPattern.java
@@ -0,0 +1,13 @@
+https://www.youtube.com/watch?v=NwaabHqPHeM&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=10
+
+- Def:  
+  Provides surrogate or placeholder for another object to control access to it. 
+  Use proxy pattern to create representative object that control access to another object 
+  which may be remote, expensive to create or in need of securing.
+- Class Diagram:
+  
+- Types
+    Remote resource
+    Virtual
+    Protection
+ 
