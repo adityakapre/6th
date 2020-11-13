ab4257812e0480b6acfd1558de812ab0c532f1e7
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 11:09:07 -0500

    Create 13_StatePattern.java

---
+++ Java/Ch 07. Object-Oriented Design/13_StatePattern.java
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=N12L5D78MAA&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=17
+
+- Def:
+  Allows object to alter its behaviour when its internal state changes. Object will appear 
+  to change its class.
+- Class diagram:
+  https://youtu.be/N12L5D78MAA?list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&t=2335
