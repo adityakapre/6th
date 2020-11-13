c4c9e3ff5a1ae49977c82daa6dac6423f5585c9b
Aditya Kapre <aditya14641@gmail.com>
2020-11-12 19:08:46 -0500

    Create 04_FactoryMethodPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/04_FactoryMethodPattern.java
@@ -0,0 +1,9 @@
+Factory method:
+https://www.youtube.com/watch?v=EcFVTgRHJLM&list=PLMygBPoOe_IV_42YFkuAECr3g7HhBXBTN&index=4
+
+- Def: 
+  The factory method pattern defines an interface for creating objects, but lets subclasses 
+  decide which class to instantiate. Factory method lets a class defer instantiation to subclasses.
+- Class diagram:
+  https://youtu.be/EcFVTgRHJLM?list=PLMygBPoOe_IV_42YFkuAECr3g7HhBXBTN&t=937
+  
