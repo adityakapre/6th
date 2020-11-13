f608257881af8173801a6473b08a91d25e141b82
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 10:45:58 -0500

    Create 09_FacadePattern.java

---
+++ Java/Ch 07. Object-Oriented Design/09_FacadePattern.java
@@ -0,0 +1,8 @@
+https://www.youtube.com/watch?v=K4FkHVO5iac&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=9
+
+- Def:
+  Provides a unified interface to set of interfaces in subsystem. 
+  Facade defines higher level interface that makes system easier to use.
+- Class diagram:
+  https://youtu.be/K4FkHVO5iac?list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&t=440
+  
