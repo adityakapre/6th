3dcb74a54aeb745c685c9a24a0b3e7094839bd4b
Aditya Kapre <aditya14641@gmail.com>
2020-11-12 19:31:16 -0500

    Create 07_AbstractFactoryPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/07_AbstractFactoryPattern.java
@@ -0,0 +1,9 @@
+https://www.youtube.com/watch?v=v-GiuMmsXj4&list=PLMygBPoOe_IV_42YFkuAECr3g7HhBXBTN&index=5
+
+- Def:
+  The Abstract Factory Pattern provides an interface for creating families of related or 
+  dependent objects without specifying their concrete classes. 
+  Often methods in abstract factory are implemented as factory methods.
+- Class diagram:
+  https://youtu.be/v-GiuMmsXj4?list=PLMygBPoOe_IV_42YFkuAECr3g7HhBXBTN&t=171
+  
