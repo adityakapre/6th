9ba4b6d4e48d42346d3566562c90cf494d6c11d5
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 11:04:29 -0500

    Create 12_CompositePattern.java

---
+++ Java/Ch 07. Object-Oriented Design/12_CompositePattern.java
@@ -0,0 +1,12 @@
+https://www.youtube.com/watch?v=EWDmWbJ4wRA&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=14
+
+-`Def:
+  Allows you to compose objects into tree structures to represent part-whole hierarchies. 
+  Composite lets client treat individual objects and composition of objects uniformly.
+  This pattern structures of objects in form of trees that contain both compositions of objects 
+  and individual objects as nodes. Using composite structures,we can apply same operations over 
+  both composite and individual objects. in other words,in most cases we can ignore differences 
+  between composition of objects and individual objects.
+- Class Diagram:
+  https://youtu.be/EWDmWbJ4wRA?list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&t=972
+  
