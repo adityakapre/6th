79a8bc8920035e99831cbcdf78cdbfdd87470c6e
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 10:56:56 -0500

    Create 10_TemplatePattern.java

---
+++ Java/Ch 07. Object-Oriented Design/10_TemplatePattern.java
@@ -0,0 +1,9 @@
+https://www.youtube.com/watch?v=7ocpwK9uesw&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=13
+
+- Def:
+  Defines skeleton of algorithm in method, deferring some steps to subclasses. 
+  Template method lets subclasses redefine certain steps of an algorithm 
+  without changing algorithm structure.
+- Class Diagram:
+  https://youtu.be/7ocpwK9uesw?list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&t=658
+  
