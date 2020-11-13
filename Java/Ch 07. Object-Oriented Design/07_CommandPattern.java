580a7037cd4243582e35029bc731cbddf77e464f
Aditya Kapre <aditya14641@gmail.com>
2020-11-13 10:26:39 -0500

    Create 07_CommandPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/07_CommandPattern.java
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=9qA5kw8dcSU&list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&index=7
+
+- Def:
+  Encapsulates a request as an object, thereby letting you parameterize other objects with 
+  different requests, queue or log requests and support undoable operations.
+- Class diagram:
+  https://youtu.be/9qA5kw8dcSU?list=PLrhzvIcii6GNjpARdnO4ueTUAVR9eMBpc&t=823
