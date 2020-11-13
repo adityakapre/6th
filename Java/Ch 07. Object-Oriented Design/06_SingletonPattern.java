d1384ecf800e546a7fc52bfe748fe6d2c2f8980b
Aditya Kapre <aditya14641@gmail.com>
2020-11-12 19:40:55 -0500

    Create 06_SingletonPattern.java

---
+++ Java/Ch 07. Object-Oriented Design/06_SingletonPattern.java
@@ -0,0 +1,6 @@
+https://www.youtube.com/watch?v=hUE_j6q0LTQ&list=PLMygBPoOe_IV_42YFkuAECr3g7HhBXBTN&index=6
+
+- Def:
+  The singleton pattern ensures that a class has only one instance and provides a global point 
+  of access to it.
+  
