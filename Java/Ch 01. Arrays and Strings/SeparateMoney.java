3f8415552e6c6c7aff397a24de21eef9958b592d
Aditya Kapre <aditya14641@gmail.com>
2020-11-15 09:34:31 -0500

    Update SeparateMoney.java

--- Java/Ch 01. Arrays and Strings/SeparateMoney.java
+++ Java/Ch 01. Arrays and Strings/SeparateMoney.java
@@ -3,3 +3,15 @@ Find a way to separate money into n parts. For example, if total money is 121,
 find a way to separate 121 into 4 closest parts and the solution would be an 
 array with elements 30, 30, 30, 31
 */
+
+public void separateMoney(int amount, int parts) {
+  int eachPart = amount/parts;
+  int remainder = amount%parts;
+  List<Integer> lsParts = new ArrayList<Integer>();
+  for(int i=0; i<parts; i++) {
+     lsParts.add(eachPart);
+  }
+  for(int i=0; i<remainder; i++) {
+      lsParts.set(i, lsParts.get(i)+1); 
+  }
+}
