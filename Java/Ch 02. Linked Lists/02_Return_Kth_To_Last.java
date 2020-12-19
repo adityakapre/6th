409152641f23b4a38793fb66a8514d7c0eaf3276
Aditya Kapre <aditya14641@gmail.com>
2020-12-19 02:18:27 -0500

    Update 02_Return_Kth_To_Last.java

--- Java/Ch 02. Linked Lists/02_Return_Kth_To_Last.java
+++ Java/Ch 02. Linked Lists/02_Return_Kth_To_Last.java
@@ -8,6 +8,47 @@ just iterate through the linked list to find this element. Because this solution
 sure that this is not what the interviewer intended.
 */
 
+/*Solution #3: Iterative
+A more optimal, but less straightforward, solution is to implement this iteratively. We can use two pointers,
+pl and p2. We place them k nodes apart in the linked list by putting p2 at the beginning and moving pl
+k nodes into the list. Then, when we move them at the same pace, pl will hit the end of the linked list after
+LENGTH - k steps. At that point, p2 will be LENGTH - k nodes into the list, or k nodes from the end.
+This algorithm takes O(n) time and 0(1) space.
+*/
+
+public class QuestionD {
+	
+	public static LinkedListNode nthToLast(LinkedListNode head, int k) {
+		LinkedListNode p1 = head;
+		LinkedListNode p2 = head;
+		
+		/* Move p1 k nodes into the list.*/
+		for (int i = 0; i < k; i++) {
+			if (p1 == null) return null; // Out of bounds
+			p1 = p1.next;
+		}
+		
+		/* Move them at the same pace. When p1 hits the end, 
+		 * p2 will be at the right element. */
+		while (p1 != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+	  	}
+	  	return p2;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {0, 1, 2, 3};
+		LinkedListNode head = AssortedMethods.createLinkedListFromArray(array);
+		for (int i = 0; i <= array.length + 1; i++) {
+			LinkedListNode node = nthToLast(head, i);
+			String nodeValue = node == null ? "null" : "" + node.data;
+			System.out.println(i + ": " + nodeValue);
+		}
+	}
+
+}
+
 /*Solution #2: Recursive
 This algorithm recurses through the linked list. When it hits the end, the method passes back a counter set
 to 0. Each parent call adds 1 to this counter. When the counter equals k, we know we have reached the kth
@@ -94,45 +135,4 @@ public class QuestionC {
 			System.out.println(i + ": " + nodeValue);
 		}
 	}
-}
-
-/*Solution #3: Iterative
-A more optimal, but less straightforward, solution is to implement this iteratively. We can use two pointers,
-pl and p2. We place them k nodes apart in the linked list by putting p2 at the beginning and moving pl
-k nodes into the list. Then, when we move them at the same pace, pl will hit the end of the linked list after
-LENGTH - k steps. At that point, p2 will be LENGTH - k nodes into the list, or k nodes from the end.
-This algorithm takes O(n) time and 0(1) space.
-*/
-
-public class QuestionD {
-	
-	public static LinkedListNode nthToLast(LinkedListNode head, int k) {
-		LinkedListNode p1 = head;
-		LinkedListNode p2 = head;
-		
-		/* Move p1 k nodes into the list.*/
-		for (int i = 0; i < k; i++) {
-			if (p1 == null) return null; // Out of bounds
-			p1 = p1.next;
-		}
-		
-		/* Move them at the same pace. When p1 hits the end, 
-		 * p2 will be at the right element. */
-		while (p1 != null) {
-			p1 = p1.next;
-			p2 = p2.next;
-	  	}
-	  	return p2;
-	}
-	
-	public static void main(String[] args) {
-		int[] array = {0, 1, 2, 3};
-		LinkedListNode head = AssortedMethods.createLinkedListFromArray(array);
-		for (int i = 0; i <= array.length + 1; i++) {
-			LinkedListNode node = nthToLast(head, i);
-			String nodeValue = node == null ? "null" : "" + node.data;
-			System.out.println(i + ": " + nodeValue);
-		}
-	}
-
 }
